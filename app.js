e7b3ac324620eb3a944bfa469d188c183be10b1a
Kyu-Sang Kim <kyu-sang.kim@student.unsw.edu.au>
2019-07-24 21:39:25 +1000

    list of course codes done

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
-const express = require('express')
-const puppeteer = require('puppeteer')
+// NODE APP INIT
+const express = require('express');
+const app = express();
+const port = 1337;
 
-const app = express()
-const port = 1337
+// MODULES
+const scraper = require('./app_modules/scrape.js')
 
-app.get('/', (req, res) => res.send('Hello World!'))
+// INDEX ROUTE
+app.get('/', async (req, res) => {
+	// call scraper with scrapeCourseList function and print
+	try {
+		let result = await scraper.scrapeCourseList();
+		await res.send(result);
+		console.log('printed message')
+	}
+	catch (err) {
+		await res.send('unexpected error has occured')
+		console.log(error(err));
+	}
+});
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => {
+	console.log(`Example app listening on port ${port}!`)
+});	
---
+++ app_modules/scrape.js
@@ -0,0 +1,35 @@
+// CONSTS
+const TIMETABLE_URL = 'http://timetable.unsw.edu.au/2019/subjectSearch.html';
+
+// MODULES
+const puppeteer = require('puppeteer');
+
+// SCRAPE FUNCTIONS
+
+// can be improved to reference by year (cbf to do it right now)
+var scrapeCourseList = (async() => {
+	try {
+		const browser = await puppeteer.launch();
+		const page = await browser.newPage();
+		await page.goto(TIMETABLE_URL);
+
+		// get raw list rows and get course code inner text from first cell
+		const result = await page.evaluate(() => {
+			let rawList = Array.from(document.querySelectorAll('.rowLowLight, .rowHighLight'));
+			let list = rawList.map(function(e) { return e.cells[0].innerText; });
+			return list;
+		})
+
+		// close the browser and return the list
+		await browser.close();
+		// console.log(result);
+		return result;
+	}
+	catch (err) {
+		console.log(error(err));
+		await browser.close(); // close the browser so no lingering instances
+	}
+})
+
+// EXPORT FUNCTIONS
+exports.scrapeCourseList = scrapeCourseList;
