e2db58267145391f862d662cfd35245abe56ab27
Marcus <z5258065@ad.unsw.edu.au>
2020-01-02 15:46:23 +1100

    theme

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import { AppBar, Toolbar, Typography } from '@material-ui/core'
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles'
+import { blue } from '@material-ui/core/colors'
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#006cef'
+    }
+  }
+})
 const App = () => (
   <div className="App">
-    <AppBar position='static'>
-      <Toolbar>
-        <Typography variant='h6'>Freerooms</Typography>
-      </Toolbar>
-    </AppBar>
+    <ThemeProvider theme={theme}>
+      <AppBar position='static'>
+        <Toolbar>
+          <Typography variant='h6'>Freerooms</Typography>
+        </Toolbar>
+      </AppBar>
+    </ThemeProvider>
+
   </div>
 )
 
