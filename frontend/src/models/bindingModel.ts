710a317c9e184dc7fbc2a638191f0775f21fe0a7
jerry <jerry.lu@mendrhub.com>
2020-05-31 00:37:48 +1000

    feat (Binding Models): Add event model for rendering to calendar

--- frontend/src/models/bindingModel.ts
+++ frontend/src/models/bindingModel.ts
@@ -18,9 +18,28 @@ class RoomInfo {
 class BookedSlots {
     Start: string;
     End: string;
+    Name: string;
 
     constructor(json: any) {
         this.Start = json.Start;
         this.End = json.End;
+        this.Name = json.Name;
+    }
+}
+
+// used to generate calendar events;
+class EventModel {
+    color: string;
+    start: string;
+    end: string;
+    name: string;
+
+    constructor(slot: BookedSlots) {
+        this.start = slot.Start;
+        this.end = slot.End;
+        this.name = slot.Name;
+        // TODO need to setup rules to convert type of 
+        // bookedSlot to color
+        this.color = "Purple";
     }
 }
