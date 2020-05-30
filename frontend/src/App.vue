8b76b6a3f60fdee7355a1615494f46eee65c7215
jerry <jerry.lu@mendrhub.com>
2020-05-31 00:37:19 +1000

    feat (Scroll Bar): Add global styling to scrollbar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -33,18 +33,35 @@
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-  background-color: #f7f7ff; 
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+    background-color: #f7f7ff; 
+  }
 
-a {
-  text-decoration: none;
-  font-weight: bold;
-}
+  a {
+    text-decoration: none;
+    font-weight: bold;
+  }
+
+  /* scroll bar styling */
+  ::-webkit-scrollbar {
+    width: 5px;
+  }
+
+  /* Track */
+  ::-webkit-scrollbar-track {
+    background: #eee;
+  }
+
+  /* Handle */
+  ::-webkit-scrollbar-thumb {
+    border-radius: 1rem;
+    background-color: #3a7bd5;
+    background-image: linear-gradient(to top, #3a7bd5 0%, #00d2ff 100%);
+  }
 </style>
