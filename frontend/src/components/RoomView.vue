3c3c5e863a9a7264aac3c2694cc1a7d853a9764c
jerry <jerry.lu@mendrhub.com>
2020-05-31 00:38:14 +1000

    feat (Room Calendar): Add preliminary calendar view

--- frontend/src/components/RoomView.vue
+++ frontend/src/components/RoomView.vue
@@ -1,41 +1,156 @@
 <template>
-    <div class="room-view">
-        Route: {{msg}}
-        params: {{params}}
-        <v-row
-          align="end"
+    <div class="RoomView">
+      <v-container>
+        <v-row           
+          no-gutters
+          align="start"
           justify="center"
-          class="grey lighten-5"
-          style="height: 300px;"
         >
-          <v-card
-            v-for="n in 3"
-            :key="n"
-            class="ma-3 pa-6"
-            outlined
-            tile
-          >
-            Room View
-          </v-card>
+          <v-col cols="3">
+            <v-card class="control-card">
+              <v-list-item>
+                <v-btn
+                  fab
+                  small
+                  absolute
+                  left
+                  :color="color"
+                  @click="$refs.calendar.prev()"
+                >
+                  <v-icon dark>mdi-chevron-left</v-icon>
+                </v-btn>
+                <v-btn
+                  fab
+                  small
+                  absolute
+                  right
+                  :color="color"
+                  @click="$refs.calendar.next()"
+                >
+                  <v-icon dark>mdi-chevron-right</v-icon>
+                </v-btn>
+              </v-list-item>
+              <v-list-item>
+                <v-menu
+                ref="startMenu"
+                v-model="startMenu"
+                :close-on-content-click="false"
+                :nudge-right="40"
+                :return-value.sync="start"
+                transition="scale-transition"
+                min-width="290px"
+                offset-y
+              >
+                <template v-slot:activator="{ on }">
+                  <v-text-field
+                    v-model="start"
+                    class="mt-3"
+                    label="Date"
+                    prepend-icon="event"
+                    dense
+                    readonly
+                    outlined
+                    hide-details
+                    v-on="on"
+                  ></v-text-field>
+                </template>
+                <v-date-picker
+                  v-model="start"
+                  no-title
+                  scrollable
+                >
+                  <v-spacer></v-spacer>
+                  <v-btn
+                    text
+                    color="primary"
+                    @click="$refs.startMenu.save(today)"
+                  >
+                    Today
+                  </v-btn>
+                  <v-btn
+                    text
+                    color="primary"
+                    @click="startMenu = false"
+                  >
+                    Cancel
+                  </v-btn>
+                  <v-btn
+                    text
+                    color="primary"
+                    @click="$refs.startMenu.save(start)"
+                  >
+                    OK
+                  </v-btn>
+                </v-date-picker>
+              </v-menu>
+              </v-list-item>
+            </v-card>
+          </v-col>
+          <v-col cols="8">
+            <v-sheet height="700">
+              <v-calendar
+                ref="calendar"
+                v-model="start"
+                :type="type"
+                :start="start"
+                :weekdays="weekdays"
+                :color = "color"
+                :first-interval="intervals.first"
+                :interval-count="intervals.count"
+                :interval-minutes="intervals.minutes"
+                :interval-height="intervals.height"
+                :events="events"
+                :event-overlap-mode="mode"
+                :event-overlap-threshold="45"
+                :event-color="getEventColor"
+              ></v-calendar>
+            </v-sheet>
+          </v-col>
         </v-row>
+      </v-container>
     </div>
 </template>
 
 <script lang="ts">
   import { Vue, Component } from 'vue-property-decorator';
+  import moment from 'moment';
 
   @Component
   export default class LocationRoomView extends Vue {
-      msg = "";
-      params = {};
+
+      intervalsDefault = {
+        // first time slot
+        first: 0,
+        // num of mins betwween time slots
+        minutes: 60,
+        // how many slots
+        count: 24,
+        // height of the individual time slots
+        height: 48,
+      }
+
+      today = moment().format('YYYY-MM-DD');
+      start = this.today;
+      events = [];
+      type = 'week';
+      mode = 'stack';
+      weekdays = [1,2,3,4,5,6,0];
+      intervals = this.intervalsDefault;
+      color = 'warning';
+
+      // initial state of time menu
+      startMenu = false;
+
       mounted() {
-          this.msg = this.$route.path;
-          this.params = this.$route.params;
+        console.log(this.start);
       }
   }
 </script>
 
 
 <style scoped>
+  .control-card {
+    margin-right: 20px;
+  }
 
 </style>
