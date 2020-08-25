c5f943e336ed52fefad72ee59c97efaa36a8b9a0
Choco3-3 <kyu-sang.kim@student.unsw.edu.au>
2020-08-25 18:23:48 +1000

    import Rohan's flawed timelist manually to save on merge conflicts

--- app_modules/scrape.js
+++ app_modules/scrape.js
@@ -108,6 +108,94 @@ var scrapeCourseCodeList = (async(typeList) => {
 	}
 });
 
+// Rohan's Flawed Timelist
+var scrapeCourseTimeList = (async (codeList) => {
+	try {
+		// remove no sandbox later when with debian
+		const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] });
+		console.log('loaded browser');
+		const page = await browser.newPage();
+		console.log('loaded newPage');
+
+		// CONSOLE.LOG CODEBLOCK
+		page.on('console', (log) => console[log._type](log._text));
+		console.log('loaded page eval console.log');
+		////////////////////////
+
+		var times = {};
+
+		// Loop through each course type
+		for (var i = 0; i < codeList.length; i++) {
+			console.log('retrieve course list for ' + codeList[i].code);
+			let courseList = codeList[i].course;
+			console.log('loaded course list');
+
+			// loop through each course of that type
+			for (var j = 0; j < courseList.length; j++) {
+				console.log('retrieve url for ' + courseList[j].code);
+				let courseURL = courseList[j].url;
+				await page.goto(courseURL);
+				console.log('loaded course URL');
+
+				// courseList and j parameters are only used for debugging
+				const result = await page.evaluate((times, courseList, j) => {
+					// xpath query to find the table for term one (hardcoded for now)
+					let xpath = "//table[tbody[tr[td[contains(text(), 'TERM ONE CLASSES - Detail')]]]]"
+					// evaluate the query and get only the first element (should only be one)
+					let element = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+
+					// if element not found we don't do anything
+					// this will occurr if a course doesn't run in term one
+					if (!element) {
+						return times;
+					}
+
+					// the table with the time data is actually after the table that we found
+					element = element.nextElementSibling;
+
+					let container = element.querySelectorAll('.rowLowLight, .rowHighLight');
+					container.forEach(function (elem) {
+						let location = elem.cells[2].innerText;
+						if (!(location in times)) {
+							times[location] = {
+								"Mon": [],
+								"Tue": [],
+								"Wed": [],
+								"Thu": [],
+								"Fri": [],
+								"Sat": [],
+								"Sun": []
+							};
+						}
+						if (times[location][elem.cells[0].innerText]) {
+							times[location][elem.cells[0].innerText].push({
+								"time": elem.cells[1].innerText,
+								"weeks": elem.cells[3].innerText
+							});
+						} else {
+							console.log("\n\n" + courseList[j].code + location + "\n\n");
+						}
+					});
+					return times;
+
+				}, times, courseList, j);
+				// changes to the object 'times' aren't saved so reassign it
+				times = result;
+			}
+
+		};
+
+		await browser.close(); // early close
+		return times;
+	}
+	catch (err) {
+		console.log(Error(err));
+		await browser.close(); // close the browser so no lingering instances
+		return Error(err);
+	}
+});
+
 // EXPORT FUNCTIONS
 exports.scrapeCourseTypeList = scrapeCourseTypeList;
 exports.scrapeCourseCodeList = scrapeCourseCodeList;
+exports.scrapeCourseTimeList = scrapeCourseTimeList;
