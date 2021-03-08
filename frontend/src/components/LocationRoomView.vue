62f7c48ed15d934fe3cdf4f7e75bea01af06911b
munjoonteo <teomunjoon101@gmail.com>
2021-03-08 11:56:36 +1100

    fixed missing route bug and redid style

--- frontend/src/components/LocationRoomView.vue
+++ frontend/src/components/LocationRoomView.vue
@@ -1,235 +1,242 @@
 <template>
-    <div class="location-room-view pb-10">
-        <!-- Header, Building Id -->
-        <v-card class="primary mx-5" style="border-radius:10px">
-          <v-row align="center">
-          <v-spacer></v-spacer>
-          <v-col
-            cols=12
-            md=4
-            class="py-3 px-10"
-          >
-              <div class="d-flex justify-left">
-                <v-avatar
-                  width="300"
-                  height="250"
-                  tile
-                  style="border-radius:10px"
-                >
-                  <v-img src="../assets/building_photos/Tyree.png"></v-img>
-                </v-avatar>
-              </div>
-            </v-col>
-            <v-col
-              cols=12
-              md=7
-              class="py-3 px-10"
-            >
-              <p class="display-3 white--text" align="left">
-                {{ buildingName }}
-              </p>
-              <!-- <p class="display-1 white--text mb-5 ml-1" align="left">K-J12</p> -->
-              <p class="subtitle-1 white--text ml-1" align="left">
-                {{ allRooms.length }} Rooms Listed<br>
-                {{ availableRooms.length }} Currently Available<br>
-                {{ unavailableRooms.length }} Available in the Next Hour
-              </p>
-            </v-col>
-          </v-row>
-        </v-card>
+  <div class="location-room-view pb-10">
+    <!-- Header, Building Id -->
+    <v-card class="primary mx-5" style="border-radius:10px">
+      <v-row align="center">
+        <v-spacer></v-spacer>
+        <v-col cols="12" md="4" class="py-3 px-10">
+          <div class="d-flex justify-left">
+            <v-avatar width="300" height="250" tile style="border-radius:10px">
+              <v-img src="../assets/building_photos/Tyree.png"></v-img>
+            </v-avatar>
+          </div>
+        </v-col>
+        <v-col cols="12" md="7" class="py-3 px-10">
+          <p class="display-3 white--text" align="left">
+            {{ buildingName }}
+          </p>
+          <!-- <p class="display-1 white--text mb-5 ml-1" align="left">K-J12</p> -->
+          <p class="subtitle-1 white--text ml-1" align="left">
+            {{ allRooms.length }} Rooms Listed<br />
+            {{ availableRooms.length }} Currently Available<br />
+            {{ unavailableRooms.length }} Available in the Next Hour
+          </p>
+        </v-col>
+      </v-row>
+    </v-card>
 
-        <!-- Filters -->
-        <v-row 
-          class="mx-5 mt-5"
-          align="end">
-          <!-- Sort -->
-          <v-col cols="12" sm="4" md="2" class="py-0 my-0"
-          style="display:none">
-            <v-select
-              :items="sort_options"
-              label="Sort"
-              :value="sort_options[0]"
-            ></v-select>
-          </v-col>
-          <!-- Hide Unavailable -->
-          <v-col cols="12" sm="6" class="py-0 my-0 pl-1">
-            <v-switch :change="updateList()" v-model="enabled" class="ma-2" label="Show Unavailable"></v-switch>
-          </v-col>
-          <v-spacer></v-spacer>
-          <!-- Date -->
-          <v-col cols="12" sm="4" md="2" class="py-0 my-0"
-          style="display:none">
-            <v-dialog
-              ref="dialog"
-              v-model="modal"
-              :return-value.sync="date"
-              persistent
-              width="290px"
-            >
-              <template v-slot:activator="{ on, attrs }">
-                <v-text-field
-                  v-model="date"
-                  label="Picker in dialog"
-                  prepend-icon="event"
-                  readonly
-                  v-bind="attrs"
-                  v-on="on"
-                ></v-text-field>
-              </template>
-              <v-date-picker v-model="date" scrollable>
-                <v-spacer></v-spacer>
-                <v-btn text color="primary" @click="modal = false">Cancel</v-btn>
-                <v-btn text color="primary" @click="$refs.dialog.save(date)">OK</v-btn>
-              </v-date-picker>
-            </v-dialog>
-          </v-col>
-          <!-- Time -->
-          <v-col cols="12" sm="4" md="2" class="py-0 my-0"
-          style="display:none">
-            <v-select
-              :items="time_options"
-              label="Time"
-              :value="time_options[0]"
-            ></v-select>
-          </v-col>
-        </v-row>
-
-        <!-- Room List -->
-        <v-row
-          class="mx-5 mt-4"
-          align="center"
-          v-for="(room, index) in listedRooms"
-          :key="index"
+    <!-- Filters -->
+    <v-row class="mx-5 mt-5" align="end">
+      <!-- Sort -->
+      <v-col cols="12" sm="4" md="2" class="py-0 my-0" style="display:none">
+        <v-select
+          :items="sort_options"
+          label="Sort"
+          :value="sort_options[0]"
+        ></v-select>
+      </v-col>
+      <!-- Hide Unavailable -->
+      <v-col cols="12" sm="6" class="py-0 my-0 pl-1">
+        <v-switch
+          :change="updateList()"
+          v-model="enabled"
+          class="ma-2"
+          label="Show Unavailable"
+        ></v-switch>
+      </v-col>
+      <v-spacer></v-spacer>
+      <!-- Date -->
+      <v-col cols="12" sm="4" md="2" class="py-0 my-0" style="display:none">
+        <v-dialog
+          ref="dialog"
+          v-model="modal"
+          :return-value.sync="date"
+          persistent
+          width="290px"
         >
-          <v-col 
-            cols="12" 
-            class="pa-0 ma-0"
-          >
-            <v-divider></v-divider>
-            <router-link :to="{name: 'room', params: { locationId : locationId, roomId: room.name}}">
-              <v-card
-                class="background"
-                flat
-              >
-                <v-card-title class="text-left">
-                  <v-icon :color="getCalendarIconColor(room.available)" class="mr-5">event_available</v-icon>
-                  <div style="width:70px"> {{ room.name }} </div>
-                  <v-divider vertical class="mx-5"></v-divider>
-                  {{ getAvailabilityText(room.available) }}
-                </v-card-title>
-              </v-card>
-            </router-link>
-          </v-col>
-        </v-row>
+          <template v-slot:activator="{ on, attrs }">
+            <v-text-field
+              v-model="date"
+              label="Picker in dialog"
+              prepend-icon="event"
+              readonly
+              v-bind="attrs"
+              v-on="on"
+            ></v-text-field>
+          </template>
+          <v-date-picker v-model="date" scrollable>
+            <v-spacer></v-spacer>
+            <v-btn text color="primary" @click="modal = false">Cancel</v-btn>
+            <v-btn text color="primary" @click="$refs.dialog.save(date)"
+              >OK</v-btn
+            >
+          </v-date-picker>
+        </v-dialog>
+      </v-col>
+      <!-- Time -->
+      <v-col cols="12" sm="4" md="2" class="py-0 my-0" style="display:none">
+        <v-select
+          :items="time_options"
+          label="Time"
+          :value="time_options[0]"
+        ></v-select>
+      </v-col>
+    </v-row>
 
-        <!-- Currently Unavailable but available later ReferenceTemplate-->
-        <!-- TODO: Implement this inside of the room list loop. 
+    <!-- Room List -->
+    <v-row
+      class="mx-5 mt-4"
+      align="center"
+      v-for="(room, index) in listedRooms"
+      :key="index"
+    >
+      <v-col cols="12" class="pa-0 ma-0">
+        <v-divider></v-divider>
+        <router-link
+          :to="{
+            name: 'room',
+            params: { locationId: locationId, roomId: room.name },
+          }"
+        >
+          <v-card class="background" flat>
+            <v-card-title class="text-left">
+              <v-icon :color="getCalendarIconColor(room.available)" class="mr-5"
+                >event_available</v-icon
+              >
+              <div style="width:70px">{{ room.name }}</div>
+              <v-divider vertical class="mx-5"></v-divider>
+              {{ getAvailabilityText(room.available) }}
+            </v-card-title>
+          </v-card>
+        </router-link>
+      </v-col>
+    </v-row>
+
+    <!-- Currently Unavailable but available later ReferenceTemplate-->
+    <!-- TODO: Implement this inside of the room list loop. 
                    Remove this row once done. -->
-        <v-row
-          class="mx-5 mt-4"
-          align="center"
-          style="display:none"
+    <v-row class="mx-5 mt-4" align="center" style="display:none">
+      <v-col cols="12" class="pa-0 ma-0">
+        <v-divider></v-divider>
+        <router-link
+          :to="{ name: 'room', params: { locationId: locationId, roomId: 4 } }"
         >
-          <v-col 
-            cols="12" 
-            class="pa-0 ma-0"
-          >
-            <v-divider></v-divider>
-            <router-link :to="{name: 'room', params: { locationId : locationId, roomId: 4}}">
-              <v-card
-                class="background"
-                flat
-              >
-                <v-card-title class="text-left">
-                  <v-icon color="warning" class="mr-5">event_note</v-icon>
-                  <span> Room 4 </span>
-                  <v-divider vertical class="mx-5"></v-divider>
-                  Available After 14:00
-                </v-card-title>
-              </v-card>
-            </router-link>
-          </v-col>
-        </v-row>
-    </div>
+          <v-card class="background" flat>
+            <v-card-title class="text-left">
+              <v-icon color="warning" class="mr-5">event_note</v-icon>
+              <span> Room 4 </span>
+              <v-divider vertical class="mx-5"></v-divider>
+              Available After 14:00
+            </v-card-title>
+          </v-card>
+        </router-link>
+      </v-col>
+    </v-row>
+  </div>
 </template>
 
 <script lang="ts">
-  import { Vue, Component, Prop } from 'vue-property-decorator';
-  import LocationService from '../services/locationService';
-  import DbService from '../services/dbService';
-  import moment from 'moment';
+import { Vue, Component, Prop } from "vue-property-decorator";
+import { Route } from "vue-router"
+import LocationService from "../services/locationService";
+import DbService from "../services/dbService";
+import moment from "moment";
 
-  @Component
-  export default class LocationRoomView extends Vue {
-    locationService = new LocationService();
-    dbService = new DbService();
+@Component
+export default class LocationRoomView extends Vue {
+  locationService = new LocationService();
+  dbService = new DbService();
 
-    // Initialisation for reactive variables.
-    msg = "";
-    params: any = null;
-    locationId = 0;
-    buildingName = "";
-    allRooms = [];
-    availableRooms = [];
-    unavailableRooms = [];
-    listedRooms= [];
-    hour = '';
+  // Initialisation for reactive variables.
+  msg = "";
+  params: any = null;
+  locationId = 0;
+  buildingName = "";
+  allRooms = [];
+  availableRooms = [];
+  unavailableRooms = [];
+  listedRooms = [];
+  hour = "";
 
+  // Filter / Sort variables.
+  enabled = true;
+  sort_options = ["Name", "Available"];
+  time_options = [
+    "0:00",
+    "1:00",
+    "2:00",
+    "3:00",
+    "4:00",
+    "5:00",
+    "6:00",
+    "7:00",
+    "8:00",
+    "9:00",
+    "10:00",
+    "11:00",
+    "12:00",
+    "13:00",
+    "14:00",
+    "15:00",
+    "16:00",
+    "17:00",
+    "18:00",
+    "19:00",
+    "20:00",
+    "21:00",
+    "22:00",
+    "23:00",
+  ];
+  date = new Date().toISOString().substr(0, 10);
+  modal = false;
 
-    // Filter / Sort variables.
-    enabled = true
-    sort_options = ['Name', 'Available'];
-    time_options = ['0:00', '1:00', '2:00', '3:00', '4:00', '5:00',
-                    '6:00', '7:00', '8:00', '9:00', '10:00', '11:00',
-                    '12:00', '13:00', '14:00', '15:00', '16:00', '17:00',
-                    '18:00', '19:00', '20:00', '21:00', '22:00', '23:00'];
-    date = new Date().toISOString().substr(0, 10);
-    modal = false;
+  async mounted() {
+    this.msg = this.$route.path;
+    this.params = this.$route.params;
+    this.hour = moment().format("hh");
+    this.locationId = this.params["locationId"];
+    this.buildingName = this.locationService.getLocationbyId(this.locationId);
+    this.allRooms = await this.dbService.getRoomsInBuilding(
+      this.buildingName,
+      this.hour
+    );
+    this.listedRooms = this.allRooms;
+    this.availableRooms = this.filterRoomsAvailable(this.allRooms, true);
+    this.unavailableRooms = this.filterRoomsAvailable(this.allRooms, false);
+  }
 
-    async mounted() {
-        this.msg = this.$route.path;
-        this.params = this.$route.params;
-        this.hour = moment().format('hh');
-        this.locationId = this.params['locationId'];
-        this.buildingName = this.locationService.getLocationbyId(this.locationId);
-        this.allRooms = await this.dbService.getRoomsInBuilding(this.buildingName, this.hour);
-        this.listedRooms = this.allRooms;
-        this.availableRooms = this.filterRoomsAvailable(this.allRooms, true);
-        this.unavailableRooms = this.filterRoomsAvailable(this.allRooms, false);
-    }
+  filterRoomsAvailable(rooms, filterAvailable) {
+    if (filterAvailable) return rooms.filter(this.checkAvailable);
+    return rooms.filter(this.checkUnavailable);
+  }
 
-    filterRoomsAvailable(rooms, filterAvailable) {
-      if (filterAvailable) return rooms.filter(this.checkAvailable);
-      return rooms.filter(this.checkUnavailable);
-    }
+  checkAvailable(room, index, array): boolean {
+    if (room.available) return true;
+    return false;
+  }
 
-    checkAvailable(room, index, array): boolean {
-      if (room.available) return true;
-      return false;
-    }
+  checkUnavailable(room, index, array): boolean {
+    if (room.available) return false;
+    return true;
+  }
 
-    checkUnavailable(room, index, array): boolean {
-      if (room.available) return false;
-      return true;
-    }
-
-    updateList() {
-      if (this.enabled == true) {
-        this.listedRooms = this.allRooms;
-      } else {
-        this.listedRooms = this.availableRooms;
-      }
-    }
-
-    // Get calendar markdown icon color depending on a room's availability.
-    getCalendarIconColor(available: boolean): string {
-      if (available == true) return "success";
-      return "error";
-    }
-
-    getAvailabilityText(available: boolean): string {
-      if (available == true) return "Available Now";
-      return "Unavailable Now";
+  updateList() {
+    if (this.enabled == true) {
+      this.listedRooms = this.allRooms;
+    } else {
+      this.listedRooms = this.availableRooms;
     }
   }
+
+  // Get calendar markdown icon color depending on a room's availability.
+  getCalendarIconColor(available: boolean): string {
+    if (available == true) return "success";
+    return "error";
+  }
+
+  getAvailabilityText(available: boolean): string {
+    if (available == true) return "Available Now";
+    return "Unavailable Now";
+  }
+}
 </script>
--- frontend/src/components/RoomView.vue
+++ frontend/src/components/RoomView.vue
@@ -1,41 +1,37 @@
 <template>
-    <div class="RoomView">
-      <v-container>
-        <v-row           
-          no-gutters
-          align="start"
-          justify="center"
-        >
-          <v-col cols="12">
-            <v-card class="control-card">
-              <v-list-item>
-                <!-- TODO: unhide buttons once code to resend request to backend
+  <div class="RoomView">
+    <v-container>
+      <v-row no-gutters align="start" justify="center">
+        <v-col cols="12">
+          <v-card class="control-card">
+            <v-list-item>
+              <!-- TODO: unhide buttons once code to resend request to backend
                             for a new week is done. -->
-                <v-btn
-                  fab
-                  small
-                  hidden
-                  absolute
-                  left
-                  :color="color"
-                  @click="$refs.calendar.prev()"
-                >
-                  <v-icon dark>mdi-chevron-left</v-icon>
-                </v-btn>
-                <v-btn
-                  fab
-                  small
-                  absolute
-                  hidden
-                  right
-                  :color="color"
-                  @click="$refs.calendar.next()"
-                >
-                  <v-icon dark>mdi-chevron-right</v-icon>
-                </v-btn>
-              </v-list-item>
-              <v-list-item class="input-item">
-                <v-menu
+              <v-btn
+                fab
+                small
+                hidden
+                absolute
+                left
+                :color="color"
+                @click="$refs.calendar.prev()"
+              >
+                <v-icon dark>mdi-chevron-left</v-icon>
+              </v-btn>
+              <v-btn
+                fab
+                small
+                absolute
+                hidden
+                right
+                :color="color"
+                @click="$refs.calendar.next()"
+              >
+                <v-icon dark>mdi-chevron-right</v-icon>
+              </v-btn>
+            </v-list-item>
+            <v-list-item class="input-item">
+              <v-menu
                 ref="startMenu"
                 v-model="startMenu"
                 :close-on-content-click="false"
@@ -58,11 +54,7 @@
                     v-on="on"
                   ></v-text-field>
                 </template>
-                <v-date-picker
-                  v-model="start"
-                  no-title
-                  scrollable
-                >
+                <v-date-picker v-model="start" no-title scrollable>
                   <v-spacer></v-spacer>
                   <v-btn
                     text
@@ -71,11 +63,7 @@
                   >
                     Today
                   </v-btn>
-                  <v-btn
-                    text
-                    :color="color"
-                    @click="startMenu = false"
-                  >
+                  <v-btn text :color="color" @click="startMenu = false">
                     Cancel
                   </v-btn>
                   <v-btn
@@ -87,122 +75,124 @@
                   </v-btn>
                 </v-date-picker>
               </v-menu>
-              </v-list-item>
-            </v-card>
+            </v-list-item>
+          </v-card>
 
-            <v-sheet height="500">
-              <v-calendar
-                ref="calendar"
-                v-model="start"
-                :type="type"
-                :start="start"
-                :weekdays="weekdays"
-                :color = "color"
-                :first-interval="intervals.first"
-                :interval-count="intervals.count"
-                :interval-minutes="intervals.minutes"
-                :interval-height="intervals.height"
-                :events="events"
-                :event-overlap-mode="mode"
-                :event-overlap-threshold="45"
-              ></v-calendar>
-            </v-sheet>
-          </v-col>
-        </v-row>
-      </v-container>
-    </div>
+          <v-sheet height="500">
+            <v-calendar
+              ref="calendar"
+              v-model="start"
+              :type="type"
+              :start="start"
+              :weekdays="weekdays"
+              :color="color"
+              :first-interval="intervals.first"
+              :interval-count="intervals.count"
+              :interval-minutes="intervals.minutes"
+              :interval-height="intervals.height"
+              :events="events"
+              :event-overlap-mode="mode"
+              :event-overlap-threshold="45"
+            ></v-calendar>
+          </v-sheet>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
 </template>
 
 <script lang="ts">
-  // We should look into spliting up the menu and the calendar into different components
-  // and using vuex to handle state control. Was Ceebs doing that so the html tags are cancer to read now...
-  import { Vue, Component } from 'vue-property-decorator';
-  import moment from 'moment';
-  import DbService from '../services/dbService';
+// We should look into spliting up the menu and the calendar into different components
+// and using vuex to handle state control. Was Ceebs doing that so the html tags are cancer to read now...
+import { Vue, Component } from "vue-property-decorator";
+import moment from "moment";
+import DbService from "../services/dbService";
+import Event from "../models/EventModel";
+import { Route } from "vue-router";
 
-  @Component
-  export default class LocationRoomView extends Vue {  
-    dbService = new DbService();
+@Component
+export default class LocationRoomView extends Vue {
+  dbService = new DbService();
 
-    params: any = [];
+  params: any = [];
 
-    roomName = ''; //TODO: modify to take actual current room name
-    bookedNameText = 'Occupied'; // Name of all bookings shown on calendar 
+  roomName = ""; //TODO: modify to take actual current room name
+  bookedNameText = "Occupied"; // Name of all bookings shown on calendar
 
-    intervalsDefault = {
-      // first time slot
-      first: 6,
-      // num of mins betwween time slots
-      minutes: 60,
-      // how many slots
-      count: 24-6,
-      // height of the individual time slots
-      height: 48,
+  intervalsDefault = {
+    // first time slot
+    first: 6,
+    // num of mins betwween time slots
+    minutes: 60,
+    // how many slots
+    count: 24 - 6,
+    // height of the individual time slots
+    height: 48,
+  };
+
+  today = moment().format("YYYY-MM-DD");
+  start = this.today;
+  events: Event[] = [];
+  type = "week";
+  mode = "stack";
+  weekdays = [1, 2, 3, 4, 5, 6, 0];
+  intervals = this.intervalsDefault;
+  color = "secondary";
+
+  // initial state of time menu
+  startMenu = false;
+
+  // Get all bookings for the room in the given time range.
+  async getEvents() {
+    const gEvents: Event[] = [];
+
+    const events = await this.getEventsFromDb();
+    for (const event of events) {
+      gEvents.push({
+        name: this.bookedNameText,
+        start: event.start,
+        end: event.end,
+      });
     }
 
-    today = moment().format('YYYY-MM-DD');
-    start = this.today;
-    events = [];
-    type = 'week';
-    mode = 'stack';
-    weekdays = [1,2,3,4,5,6,0];
-    intervals = this.intervalsDefault;
-    color = 'secondary';
-
-    // initial state of time menu
-    startMenu = false;
-
-    // Get all bookings for the room in the given time range.
-    async getEvents () {
-      const gEvents = [];
-
-      const events = await this.getEventsFromDb();
-      for (const event of events) {
-        gEvents.push({
-          name: this.bookedNameText,
-          start: event.start,
-          end:  event.end,
-
-        });
-      }
-      
-      return gEvents;
-    }
-
-    async getEventsFromDb() {
-      const startTime = moment().format('YYYY-MM-DD');
-      const endTime = moment().format('YYYY-MM-DD');
-      const result = await this.dbService.getRoomBookingsInTimeRange(this.roomName, startTime, endTime);
-      return result;
-    }
-
-    async mounted() {
-      this.params = this.$route.params;
-      this.roomName = this.params['roomId'];
-      if (this.roomName == null) this.roomName = '';
-      this.events = await this.getEvents();
-    }
+    return gEvents;
   }
+
+  async getEventsFromDb() {
+    const startTime = moment().format("YYYY-MM-DD");
+    const endTime = moment().format("YYYY-MM-DD");
+    const result = await this.dbService.getRoomBookingsInTimeRange(
+      this.roomName,
+      startTime,
+      endTime
+    );
+    return result;
+  }
+
+  async mounted() {
+    this.params = this.$route.params;
+    this.roomName = this.params["roomId"];
+    if (this.roomName == null) this.roomName = "";
+    this.events = await this.getEvents();
+  }
+}
 </script>
 
-
 <style scoped>
-  .control-card {
-    margin-right: 20px;
-    width:100%;
-    margin-bottom: 10px;
-  }
+.control-card {
+  margin-right: 20px;
+  width: 100%;
+  margin-bottom: 10px;
+}
 
-  .input-item {
-    padding-bottom: 20px;
-  }
-
+.input-item {
+  padding-bottom: 20px;
+}
 </style>
 
 <style>
-
-  .v-btn--fab.v-size--default {
-    height: 30px;
-    width: 30px;
-  }
+.v-btn--fab.v-size--default {
+  height: 30px;
+  width: 30px;
+}
 </style>
