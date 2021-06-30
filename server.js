1af833673fc6439cdccd1f16fa20736460074508
damo190 <damjan.stevanoski17@gmail.com>
2021-06-30 14:40:04 +1000

    added buildings .json and /buildings endpoint functionality

--- server.js
+++ server.js
@@ -10,11 +10,32 @@ var dataJson = require(dataJsonPath);
 // Route to get all buildings
 app.get("/buildings", async (req, res) => {
   try {
-    res.send("Requested all buildings");
+    console.log(`Requested all buildings`);
+    let ret = '{ "buildings" : [';
+    let i = 1;
+    for (building in dataJson["U1"]) {
+      ret = ret + '{';
+      ret = ret + '"name" : "' + buildingDataJson[building]["name"] + '" ,';
+      ret = ret + '"id" : "' + buildingDataJson[building]["id"] + '" ,';
+      ret = ret + '"img" : "' + buildingDataJson[building]["img"] + '"';
+      ret = ret + '}';
+      //console.log(Object.keys(dataJson["U1"]).length);
+      if (i != Object.keys(dataJson["U1"]).length) {
+        ret = ret + ',';
+      }
+      
+      
+      i++;
+    }
+    ret = ret + ']}';
+    //console.log(ret);
+    console.log(JSON.parse(ret));
+    res.send(JSON.parse(ret));
   } catch (err) {
-    res.send("Buildings data error");
+    res.send(`Failed to send all buildings`);
     console.log(Error(err));
   }
+
 });
 
 // Route to get status of all rooms in a particular building
