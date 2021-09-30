afc302fac8d6fa9b5817ba8b374f830f1d7fa47d
Mun Joon Teo <teomunjoon101@gmail.com>
2021-09-30 20:53:39 +1000

    Set current date to today upon load

--- frontend/src/views/RoomView.vue
+++ frontend/src/views/RoomView.vue
@@ -4,8 +4,8 @@
       <v-row no-gutters align="start" justify="center">
         <v-col cols="12">
           <v-card class="control-card">
-            <!--Go forward or back one week-->
-            <!-- TODO: unhide buttons once code to resend request to backend for a new week is done. -->
+            <!-- Go forward or back one week - BROKEN.
+            Changes the date in the datepicker + calendar but events are not updated with the current date
             <v-list-item>
               <v-btn
                 fab
@@ -29,7 +29,7 @@
               >
                 <v-icon dark>mdi-chevron-right</v-icon>
               </v-btn>
-            </v-list-item>
+            </v-list-item> -->
             <!--Date Picker-->
             <v-list-item class="input-item">
               <v-menu
@@ -60,7 +60,10 @@
                   <v-btn
                     text
                     :color="color"
-                    @click="$refs.startMenu.save(today); updateDateTime();"
+                    @click="
+                      $refs.startMenu.save(today);
+                      updateDateTime();
+                    "
                   >
                     Today
                   </v-btn>
@@ -70,7 +73,10 @@
                   <v-btn
                     text
                     :color="color"
-                    @click="$refs.startMenu.save(start); updateDateTime();"
+                    @click="
+                      $refs.startMenu.save(start);
+                      updateDateTime();
+                    "
                   >
                     OK
                   </v-btn>
@@ -108,7 +114,7 @@
 // TODO Use vuex to handle state
 import { Vue, Component } from "vue-property-decorator";
 import DbService from "../services/dbService";
-import {DateTime} from "luxon";
+import { DateTime } from "luxon";
 import { EventModel } from "../models/BindingModel";
 import { Route } from "vue-router";
 
@@ -118,7 +124,7 @@ export default class LocationRoomView extends Vue {
 
   params: any = [];
 
-  locationId = ""; 
+  locationId = "";
   roomId = ""; // TODO: modify to take actual current room name
   bookedNameText = "Occupied"; // Name of all bookings shown on calendar
   intervalsDefault = {
@@ -130,7 +136,6 @@ export default class LocationRoomView extends Vue {
     count: 24 - 6,
     // Height of the individual time slots
     height: 48,
-
   };
 
   today = DateTime.now().toFormat("yyyy-MM-dd");
@@ -147,26 +152,25 @@ export default class LocationRoomView extends Vue {
 
   // Get all bookings for the room in the given time range.
   async getEventsFromDb() {
-    
-    const startTime = DateTime.fromFormat(this.start, "yyyy-MM-dd").set({weekday: 1}).toFormat("yyyy-MM-dd");
-    
-    const endTime = DateTime.fromFormat(startTime, "yyyy-MM-dd").plus({days: 6}).toFormat("yyyy-MM-dd");
+    const startTime = DateTime.fromFormat(this.start, "yyyy-MM-dd")
+      .set({ weekday: 1 })
+      .toFormat("yyyy-MM-dd");
 
-    //console.log(startTime, endTime);
+    const endTime = DateTime.fromFormat(startTime, "yyyy-MM-dd")
+      .plus({ days: 6 })
+      .toFormat("yyyy-MM-dd");
+
     const result = await this.dbService.getRoomBookingsInTimeRange(
       this.locationId,
       this.roomId,
       startTime,
-      endTime,
+      endTime
     );
-    // console.log("aloha");
-    // console.log(result);
     return result;
   }
 
   // Add name and colour to events
   async getEvents() {
-    
     const allEvents: EventModel[] = [];
 
     const events = await this.getEventsFromDb();
@@ -178,21 +182,23 @@ export default class LocationRoomView extends Vue {
         color: "",
       });
     }
-    console.log(events);
-    console.log(allEvents);
     return allEvents;
   }
 
   async mounted() {
     this.locationId = this.$route.params["locationId"];
     this.roomId = this.$route.params["roomId"];
-    this.start = this.$route.params["datetime"]
-    //console.log("we are looking at " + this.locationId + " at room " + this.roomId);
+    this.start = this.$route.params["datetime"];
+
     if (this.roomId == null) this.roomId = "";
+
+    this.today = DateTime.now().toFormat("yyyy-MM-dd");
+    this.start = this.today;
+
     this.events = await this.getEvents();
   }
+
   async updateDateTime() {
-    
     this.events = await this.getEvents();
   }
 }
