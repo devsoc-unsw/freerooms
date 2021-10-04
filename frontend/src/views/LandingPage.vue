a714cd6c7abd4cbb0d6e723c8d65dadf8b8ed1c4
Mun Joon Teo <teomunjoon101@gmail.com>
2021-10-04 21:46:40 +1100

    Initial text for dropdown

--- frontend/src/views/LandingPage.vue
+++ frontend/src/views/LandingPage.vue
@@ -2,7 +2,7 @@
   <div class="room-view">
     <v-expansion-panels class="pa-5 my-3" align="center">
       <v-expansion-panel class="primary pa-5" style="border-radius:10px">
-        <!-- 
+        <!--
         TODO: Header is the text that will display before expansion, you can move the text around to see what is most appropriate.
       -->
         <v-expansion-panel-header class="d-flex flex-column">
@@ -11,17 +11,39 @@
           </p>
           <div class="subtitle-3 white--text" align="center">
             <p>
-              This is a tool to help students find vacant rooms across our UNSW
-              campuses.
+              FreeRooms is a tool helping UNSW students find their dream study
+              location within the campus. We have many features on the way,
+              including a map with the real-time availabilities of each room in a
+              UNSW building. It was created by CSESoc Projects - a place for
+              student-led projects where you can learn something new and make some
+              friends along the way. FreeRooms is free and open-source.
             </p>
             <p>
-              Expand to read more info about us :)
+              Expand to read more!
             </p>
           </div>
         </v-expansion-panel-header>
         <v-expansion-panel-content>
-          <p class="subtitle-3 white--text">
-            Some random content
+          <p>
+            How it works:
+            <ul>
+              <li>
+                Click on a building you want to study at and the availabilities of different rooms in that building will be listed on the page.
+              </li>
+              <li>
+                If you want to see the availability of the room over a week, click on the calendar icon next to the room.
+              </li>
+              <li>
+                If you want to plan your university day, you can change the time and day to see the availability of rooms in the future.
+              </li>
+            </ul>
+          </p>
+          <p>
+            Features:
+            <ul>
+              <li>Plan ahead with our calendar, and have regular weekly meetings with your study groups!</li>
+              <li>Dark mode 😎</li>
+            </ul>
           </p>
         </v-expansion-panel-content>
       </v-expansion-panel>
