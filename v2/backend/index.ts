4e6ebbe6ac733de9fe3153d8160cce319416050c
Mun Joon Teo <teomunjoon101@gmail.com>
2021-10-11 19:48:53 +1100

    Base framework

--- v2/backend/index.ts
+++ v2/backend/index.ts
@@ -3,6 +3,9 @@ import express from "express";
 const app = express();
 const PORT = 3000;
 
+// index.ts only job is extracting the parameters from the url. There should be no other logic
+// Pass these parameters to service.ts and then `return res.send()` the result
+
 app.get("/buildings", (req: express.Request, res: express.Response) => {
   res.send("Hello world!");
 });
---
+++ v2/backend/service.ts
@@ -0,0 +1,2 @@
+// All logic should go in here.
+// All functions in this file should take in some params from index.ts and spit out an object of some sort
