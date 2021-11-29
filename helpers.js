518fb5c07a1ebde4b458ba597b3e96666f5641f7
Mun Joon Teo <teomunjoon101@gmail.com>
2021-11-29 22:42:08 +1100

    copied backend over from v2

---
+++ helpers.js
@@ -0,0 +1,78 @@
+import axios from "axios";
+import pkg from "jsdom";
+const { JSDOM } = pkg;
+
+const SCRAPER_URL =
+  "https://timetable.csesoc.unsw.edu.au/api/terms/2021-T1/freerooms/";
+
+export const getData = async () => {
+  const res = await axios.get(SCRAPER_URL);
+  const data = await res.data;
+  return data;
+};
+
+// Gets all the room codes for rooms in UNSW by parsing the HTML with regex (please excuse my cardinal sin)
+export const getAllRoomIDs = async () => {
+  const ROOM_URL =
+    "https://www.learningenvironments.unsw.edu.au/find-teaching-space?building_name=&room_name=&page=";
+
+  const MAX_PAGES = 13;
+
+  const ROOM_REGEX = /^[A-Z]-[A-Z][0-9]{1,2}-[A-Z]{0,2}[0-9]{1,4}[A-Z]{0,1}$/;
+  // One letter - campus ID, e.g. K for Kensington
+  // One letter followed by one or two numbers for grid reference e.g. D16 or F8
+  // Zero, one or two letters for the floor then between one to four numbers for the room number
+  // Library rooms may end in a letter
+  // Zero letter floor - 313
+  // One letter floor - M18
+  // Two letter floor - LG19
+
+  let roomIDs = [];
+
+  for (let i = 0; i < MAX_PAGES; i++) {
+    const response = await axios.get(ROOM_URL + i);
+    const data = await response.data;
+
+    const htmlDoc = new JSDOM(data);
+    const rawRoomIDs =
+      htmlDoc.window.document.getElementsByClassName("field-item");
+
+    if (!rawRoomIDs) return roomIDs;
+
+    const cleanRoomIDs = [];
+
+    for (let j = 0; j < rawRoomIDs.length; j++) {
+      let roomID = rawRoomIDs.item(j)?.innerHTML;
+      if (roomID && ROOM_REGEX.test(roomID)) {
+        cleanRoomIDs.push(roomID);
+      }
+    }
+
+    roomIDs = roomIDs.concat(cleanRoomIDs);
+  }
+
+  return roomIDs;
+};
+
+// Gets the week number from the date
+export const getWeek = (data, date) => {
+  // In 'DD/MM/YYYY' format
+  const termStart = data["termStart"];
+
+  const termStartDate = new Date(termStart);
+  const today = date;
+
+  const diff = today.getTime() - termStartDate.getTime();
+
+  let daysPastTerm = diff / (1000 * 60 * 60 * 24);
+
+  // Integer division to get term number
+  // Ceil is used because week numbers start from 1 not 0
+  return Math.ceil(daysPastTerm / 7);
+};
+
+// Parses the provided datetime from the request params
+export const getDate = (datetime) => {
+  let timestamp = Date.parse(datetime);
+  return isNaN(timestamp) ? null : new Date(datetime);
+};
--- server.js
+++ server.js
@@ -1,132 +1,67 @@
-// Initialising application using Express
 import express from "express";
 
-// Helper functions
+import { getDate } from "./helpers.js";
 import {
-  getData,
-  getAllRooms,
-  retrieveRoomStatus,
-} from "./timetableCalculations.js";
-
-import buildingData from "./buildings.js";
+  getAllRoomStatus,
+  getAllBuildings,
+  getRoomAvailability,
+} from "./service.js";
 
 const app = express();
-const port = 3000;
+const PORT = 3000;
 
-// Route to get all buildings
+const errorHandler = (res, message) => {
+  console.error(`Error: ${message}`);
+  res.send({
+    message: message,
+    status: 400,
+  });
+};
+
+// Route to get all the buildings
 app.get("/buildings", async (req, res) => {
   try {
-    let buildings = { buildings: [] };
-
-    for (let building in buildingData) {
-      let iter = {};
-      iter["name"] = buildingData[building]["name"];
-      iter["id"] = buildingData[building]["id"];
-      iter["img"] = buildingData[building]["img"];
-      buildings["buildings"].push(iter);
-    }
-
-    res.send(buildings);
-  } catch (err) {
-    res.send(`Failed to send all buildings`);
-    console.log(Error(err));
+    const buildingData = await getAllBuildings();
+    const data = { buildings: buildingData };
+    res.send(data);
+  } catch (error) {
+    errorHandler(res, error.message);
   }
 });
 
-// Route to get status of all rooms in a particular building
-app.get("/buildings/:buildingId", async (req, res) => {
+// Route to get all the rooms in a particular building
+app.get("/buildings/:buildingID", async (req, res) => {
+  const { buildingID } = req.params;
+  const datetimeString = req.query.datetime;
+
   try {
-    const data = await getData();
+    const datetime = datetimeString ? getDate(datetimeString) : new Date();
 
-    let buildingID = req.params.buildingId;
-
-    if (!(buildingID in buildingData)) {
-      res.send({
-        message: "invalid building ID",
-        status: 400,
-      });
-
+    if (datetime === null) {
+      res.send({ message: "Invalid date", status: 400 });
       return;
     }
 
-    //Get current date
-    let currDate = new Date();
-
-    //Check if datetime query was passed
-    if (req.query.datetime) {
-      let timestamp = Date.parse(req.query.datetime);
-
-      if (isNaN(timestamp)) {
-        res.send({
-          message: "invalid date",
-          status: 400,
-        });
-
-        return;
-      } else {
-        //If it is a valid date, change currDate to intead be the datetime passed in the query
-        currDate = new Date(req.query.datetime);
-      }
-    }
-
-    //Create JS object from JSON file
-    let buildingObj = data[buildingID];
-
-    //Pass through to function to get all rooms and their status into required roomStatus
-    let roomStatus = await retrieveRoomStatus(
-      buildingID,
-      buildingObj,
-      currDate
-    );
-    res.send(roomStatus);
-  } catch (err) {
-    res.send("Building rooms data error");
-    console.log(Error(err));
+    const roomData = await getAllRoomStatus(buildingID, datetime);
+    const data = { rooms: roomData };
+    res.send(data);
+  } catch (error) {
+    errorHandler(res, error.message);
   }
 });
 
-// Route to get availability of a particular room in a particular building
-app.get("/buildings/:buildingId/:roomId", async (req, res) => {
+// Route to get the availability of a particular room in a particular building
+app.get("/buildings/:buildingID/:roomNumber", async (req, res) => {
+  const { buildingID, roomNumber } = req.params;
+
   try {
-    const data = await getData();
-    const allRooms = await getAllRooms();
-
-    let buildingID = req.params.buildingId;
-    let roomID = req.params.roomId;
-
-    //Ensure building ID is valid
-    if (!(buildingID in buildingData)) {
-      res.send({
-        message: "invalid building ID",
-        status: 400,
-      });
-
-      return;
-    }
-
-    //Ensure room ID is valid
-    if (!data[buildingID] || !(roomID in data[buildingID])) {
-      if (allRooms.includes(buildingID + "-" + roomID)) {
-        return [];
-      } else {
-        res.send({
-          message: "invalid room ID",
-          status: 400,
-        });
-
-        return;
-      }
-    }
-
-    //Generate return data straight from the JSON data
-    let availabilities = data[buildingID][roomID];
-    res.send(availabilities);
-  } catch (err) {
-    res.send("Room availability data error");
-    console.log(Error(err));
+    const data = await getRoomAvailability(buildingID, roomNumber);
+    res.send(data);
+  } catch (error) {
+    errorHandler(res, error.message);
   }
 });
 
-app.listen(port, () => {
-  console.log(`Freerooms backend now listening on port ${port}!`);
+app.listen(PORT, () => {
+  console.log(`Freerooms backend now listening on port ${PORT}!`);
 });
