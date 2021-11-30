00f79561539f8af05e6abc4ed866e6046c15d333
Mun Joon Teo <teomunjoon101@gmail.com>
2021-11-30 17:47:49 +1100

    turned info box into a modal

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -4,6 +4,10 @@ import store from "./store";
 import vuetify from "./plugins/vuetify";
 import router from "./routes/routes";
 
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+Vue.component("font-awesome-icon", FontAwesomeIcon);
+
 Vue.config.productionTip = false;
 
 new Vue({
