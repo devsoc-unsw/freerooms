de01fbc82719c0ffa30072706700fc2bee5c2dd4
Eric Liu <me@fxlui.com>
2022-07-14 18:20:17 +1000

    fix button typing

--- v2/frontend/components/Button.tsx
+++ v2/frontend/components/Button.tsx
@@ -1,27 +1,17 @@
 import Button, { ButtonProps } from "@mui/material/Button";
-import React from "react";
+import { styled } from "@mui/material/styles";
 
-const StyledButton: React.FC<{
-  children: React.ReactNode;
-  props?: ButtonProps;
-}> = ({ children, props }) => (
-  <Button
-    {...props}
-    sx={(theme) => ({
-      borderRadius: "10px",
-      padding: theme.spacing(1, 3),
-      backgroundColor: "#eee",
-      color: "#333",
-      textTransform: "none",
-      transition: "all 0.1s ease-in-out",
-      "&:hover": {
-        backgroundColor: theme.palette.primary.main,
-        color: "#fff",
-      },
-    })}
-  >
-    {children}
-  </Button>
-);
+const StyledButton = styled(Button)<ButtonProps>(({ theme }) => ({
+  borderRadius: "10px",
+  padding: theme.spacing(1, 3),
+  backgroundColor: "#eee",
+  color: "#333",
+  textTransform: "none",
+  transition: "all 0.1s ease-in-out",
+  "&:hover": {
+    backgroundColor: theme.palette.primary.main,
+    color: "#fff",
+  },
+}));
 
 export default StyledButton;
