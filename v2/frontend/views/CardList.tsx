ca46b6699f5658e12b74f5425b811a1b11896f42
Franco Reyes <z5359122@ad.unsw.edu.au>
2022-10-09 14:26:39 +1100

    Card shuffle animation

--- v2/frontend/views/CardList.tsx
+++ v2/frontend/views/CardList.tsx
@@ -1,11 +1,38 @@
 
 import React from "react";
-import {Building, BuildingReturnData, RoomsReturnData } from "../types";
+import { Building, BuildingReturnData, RoomsReturnData } from "../types";
+
+import { styled } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
+import FlipMove from "react-flip-move";
 
 import BuildingCard from "../components/BuildingCard";
 
 const INITIALISING = -2;
 const FAILED = -1;
+
+const FlipMoveGrid = styled(FlipMove)(({ theme }) => ({
+  width: "100%",
+  display: "grid",
+  gridTemplateColumns: "repeat(auto-fill, minmax(300px, 1fr))",
+  gridGap: "20px",
+}));
+
+const FlippableCard = React.forwardRef<HTMLDivElement, {
+  building: Building;
+  setBuilding: (bldg: Building) => void;
+  freerooms: number;
+}>(({ building, setBuilding, freerooms }, ref) => (
+  <div ref={ref}>
+    <BuildingCard
+      building={building}
+      setBuilding={setBuilding}
+      freerooms={freerooms}
+    />
+  </div>
+));
 
 const CardList: React.FC<{
   data: BuildingReturnData;
@@ -18,13 +45,15 @@ const CardList: React.FC<{
   const [buildings, setBuildings] = React.useState<Building[]>([...data.buildings]);
 
   React.useEffect(() => {
+    if (statusData === undefined) return;
+
     // Filter any out that dont start with query
     // If hideUnavailable is true, filter any that have no available rooms
-    const displayedBuildings = [...data.buildings].filter((building) => 
-        building.name.toLowerCase().startsWith(searchQuery.toLowerCase()) &&
-        (!hideUnavailable || countFreerooms(statusData, building.id))
+    const displayedBuildings = [...data.buildings].filter((building) =>
+      building.name.toLowerCase().startsWith(searchQuery.toLowerCase()) &&
+      (!hideUnavailable || countFreerooms(statusData, building.id))
     );
-    
+
     // Sort the displayed buildings
     displayedBuildings.sort((a, b) => {
       switch (sortOrder) {
@@ -39,33 +68,26 @@ const CardList: React.FC<{
         case "mostRooms":
           return countFreerooms(statusData, b.id) - countFreerooms(statusData, a.id);
         case "nearest":
-          // idk lol
+        // idk lol
         default:
           return 0
       }
     });
-  
+
     setBuildings(displayedBuildings);
   }, [searchQuery, hideUnavailable, sortOrder, statusData]);
 
   return (
-    <div
-      style={{
-        width: "100%",
-        display: "grid",
-        gridTemplateColumns: "repeat(auto-fill, minmax(300px, 1fr))",
-        gridGap: "20px",
-      }}
-    >
+    <FlipMoveGrid>
       {buildings.map((building) => (
-        <BuildingCard
-          building={building}
+        <FlippableCard
           key={building.id}
+          building={building}
           setBuilding={setBuilding}
           freerooms={countFreerooms(statusData, building.id)}
         />
       ))}
-    </div>
+    </FlipMoveGrid>
   );
 }
 
