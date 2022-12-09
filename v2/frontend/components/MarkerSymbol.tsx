da2980481b6e644ed88466cc7023da6575c811c4
Britain1680 <79031614+Britain1680@users.noreply.github.com>
2022-12-09 16:45:12 +1100

    refactoring code

--- v2/frontend/components/MarkerSymbol.tsx
+++ v2/frontend/components/MarkerSymbol.tsx
@@ -1,31 +1,34 @@
+import SimpleRenderer from "esri/renderers/SimpleRenderer";
+import SimpleMarkerSymbol from "esri/symbols/SimpleMarkerSymbol";
+
 // If building is not yet clicked, it will be orange with a white outline 
-const openFeature = {
-  "type": "simple",
-  "symbol": {
-    "type": "simple-marker",
-    "style": "circle",
-    "size": "18px",
-    "color": "#C64D2A", 
-    "outline": {  
-      "color": "white",
-      "width": 4
-    }
+const simpleMarker = new SimpleMarkerSymbol({
+  style: "circle", 
+  size: "18px",
+  color: "#C64D2A", 
+  outline: {
+    color: "white", 
+    width: 4
   }
-};
+});
+
+const openFeature = new SimpleRenderer({
+  symbol: simpleMarker
+});
 
 // If building is clicked, it will be black with a white outline
-const clickedFeature = {
-  "type": "simple",
-  "symbol": {
-    "type": "simple-marker",
-    "style": "circle",
-    "size": "18px",
-    "color": "black", 
-    "outline": {  
-      "color": "white",
-      "width": 4
-    }
+const simpleMarker2 = new SimpleMarkerSymbol({
+  style: "circle", 
+  size: "18px",
+  color: "black", 
+  outline: {
+    color: "white", 
+    width: 4
   }
-};
+});
+
+const clickedFeature = new SimpleRenderer({
+  symbol: simpleMarker2
+});
 
 export { clickedFeature,openFeature }
