7b27433bcec22f7d0f7b44e557caeb9ffa1087e6
Britain1680 <79031614+Britain1680@users.noreply.github.com>
2022-12-09 17:44:36 +1100

    Revert "refactoring code"
    This reverts commit da2980481b6e644ed88466cc7023da6575c811c4.

--- v2/frontend/components/BaseMap.tsx
+++ v2/frontend/components/BaseMap.tsx
@@ -1,15 +1,5 @@
+import { loadModules } from "esri-loader";
 import React, { useEffect } from "react";
-
-import Map from "esri/Map";
-import MapView from "esri/views/MapView";
-import esriConfig from "esri/config";
-import FeatureLayer from "esri/layers/FeatureLayer";
-import Graphic from "esri/Graphic";
-import Basemap from "esri/Basemap";
-import VectorTileLayer from "esri/layers/VectorTileLayer";
-import Track from "esri/widgets/Track";
-import LabelClass from "esri/layers/support/LabelClass";
-import Search from "esri/widgets/Search";
 
 import { Building, BuildingReturnData } from "../types";
 import { clickedFeature,openFeature } from "./MarkerSymbol";
@@ -19,122 +9,148 @@ const Mapping: React.FC<{
   setCurrentBuilding: (building: Building) => void;
 }> = ({ buildingData, setCurrentBuilding }) => {
   useEffect(() => {
-    const basemap = new Basemap({
-      baseLayers: [
-        new VectorTileLayer({
-          portalItem: {
-            id: "df9c45bd660c42e99231f5b197386599",
-          },
-        }),
-      ],
-    });
+    loadModules([
+      "esri/Map",
+      "esri/views/MapView",
+      "esri/config",
+      "esri/layers/FeatureLayer",
+      "esri/Graphic",
+      "esri/Basemap",
+      "esri/layers/VectorTileLayer",
+      "esri/widgets/Track",
+      "esri/widgets/Search",
+    ])
+      .then(
+        ([
+          Map,
+          MapView,
+          esriConfig,
+          FeatureLayer,
+          Graphic,
+          Basemap,
+          VectorTileLayer,
+          Track,
+          Search,
+        ]) => {
+          const basemap = new Basemap({
+            baseLayers: [
+              new VectorTileLayer({
+                portalItem: {
+                  id: "df9c45bd660c42e99231f5b197386599",
+                },
+              }),
+            ],
+          });
 
-    const map = new Map({
-      basemap: basemap,
-    });
+          const map = new Map({
+            basemap: basemap,
+          });
 
-    const view = new MapView({
-      map: map,
-      center: [151.231, -33.917], // Longitude, latitude
-      zoom: 16, // Zoom level
-      container: "viewDiv", // Div element
-    });
+          const view = new MapView({
+            map: map,
+            center: [151.231, -33.917], // Longitude, latitude
+            zoom: 16, // Zoom level
+            container: "viewDiv", // Div element
+          });
 
-    // tracking widget
-    const track = new Track({
-      view: view,
-    });
+          // tracking widget
+          const track = new Track({
+            view: view,
+          });
 
-    view.ui.add(track, "top-left");
-    view.when(() => {
-      track.start();
-    });
+          view.ui.add(track, "top-left");
+          view.when(() => {
+            track.start();
+          });
 
-    esriConfig.apiKey =
-      "AAPK7fbfd710c5e443e28d309fc3ba297ceb_Wb-Lg6smsxOnafMSRQLTx3WwDI-mv9jKXig8AECw0pTh06C_uyS40dTDpgPIDMd";
+          esriConfig.apiKey =
+            "AAPK7fbfd710c5e443e28d309fc3ba297ceb_Wb-Lg6smsxOnafMSRQLTx3WwDI-mv9jKXig8AECw0pTh06C_uyS40dTDpgPIDMd";
 
-    
-    const buildingLabel = new LabelClass({
-      symbol: {
-        type: "text",
-        color: "#61432C",
-        haloColor: "white",
-        haloSize: "2px",
-        font: {
-          size: "10px",
-          family: "Noto Sans",
-          style: "normal",
-          weight: "normal",
-        },
-      },
+          const buildingLabel = {
+            symbol: {
+              type: "text",
+              color: "#61432C",
+              haloColor: "white",
+              haloSize: "2px",
+              font: {
+                size: "10px",
+                family: "Noto Sans",
+                style: "normal",
+                weight: "normal",
+              },
+            },
 
-      labelPlacement: "above-center",
-      labelExpressionInfo: {
-        expression: "$feature.building_name",
-      },
-    });
+            labelPlacement: "above-center",
+            labelExpressionInfo: {
+              expression: "$feature.building_name",
+            },
+          };
 
-    const layer = new FeatureLayer({
-      url: "https://services7.arcgis.com/2PcAgU0oUMmfCqzW/arcgis/rest/services/unsw_map_feature_layer/FeatureServer/0",
-      renderer: openFeature,
-      labelingInfo: [buildingLabel],
-    });
+          const layer = new FeatureLayer({
+            url: "https://services7.arcgis.com/2PcAgU0oUMmfCqzW/arcgis/rest/services/unsw_map_feature_layer/FeatureServer/0",
+            renderer: openFeature,
+            labelingInfo: [buildingLabel],
+          });
 
-    map.add(layer);
+          map.add(layer);
 
-    const sources = [{
-      layer: layer,
-      maxResults: 5,
-      searchFields: ["building_name"],
-      // displayField: "POI",
-      name: "Buiding Name",
-    }, {
-      name: "Location Service",
-      apiKey:
-        "AAPK7fbfd710c5e443e28d309fc3ba297ceb_Wb-Lg6smsxOnafMSRQLTx3WwDI-mv9jKXig8AECw0pTh06C_uyS40dTDpgPIDMd",
-      singleLineFieldName: "SingleLine",
-      url: "https://geocode-api.arcgis.com/arcgis/rest/services/World/GeocodeServer",
-    }];
-    
-    // search widget
-    const search = new Search({
-      view: view,
-      allPlaceholder: "Building Name",
-      includeDefaultSources: false,
-      sources: sources,
-    });
+          // search widget
+          const search = new Search({
+            view: view,
+            allPlaceholder: "Building Name",
+            includeDefaultSources: false,
+            sources: [
+              {
+                name: "Building Name",
+                layer: layer,
+                searchFields: ["building_name"],
+                exactMatch: false,
+              },
+              {
+                name: "Location Service",
+                apiKey:
+                  "AAPK7fbfd710c5e443e28d309fc3ba297ceb_Wb-Lg6smsxOnafMSRQLTx3WwDI-mv9jKXig8AECw0pTh06C_uyS40dTDpgPIDMd",
+                singleLineFieldName: "SingleLine",
+                url: "https://geocode-api.arcgis.com/arcgis/rest/services/World/GeocodeServer",
+              },
+            ],
+          });
 
-    view.ui.add(search, {
-      position: "top-right",
-    });
+          view.ui.add(search, {
+            position: "top-right",
+          });
 
-    view.on("click", async (event: any) => {
-      const response = await view.hitTest(event.screenPoint);
-      console.log(response.results[0]);
-      const graphic = response.results[0].graphic;
+          view.on("click", async (event: any) => {
+            const response = await view.hitTest(event.screenPoint);
+            const graphic = response.results[0].graphic;
 
-      const attributes = graphic.attributes;
-      if (attributes) {
-        const building = attributes.building_name;
+            const attributes = graphic.attributes;
+            if (attributes) {
+              const building = attributes.building_name;
 
-        if (building) {
-          const buildingFound = buildingData.buildings.find(
-            (b: Building) => b.name === building
-          );
+              if (building) {
+                const buildingFound = buildingData.buildings.find(
+                  (b: Building) => b.name === building
+                );
 
-          if (buildingFound) {
-            setCurrentBuilding(buildingFound);
-          }
+                if (buildingFound) {
+                  setCurrentBuilding(buildingFound);
+                }
+              }
+            }
+
+            const geometry = graphic.geometry;
+            if (geometry && geometry.type == "point") {
+              const graphicLayer = new Graphic(geometry, clickedFeature);
+              view.graphics.removeAll();
+              view.graphics.add(graphicLayer);
+            }
+          });
         }
-      }
-
-      const geometry = graphic.geometry;
-      if (geometry && geometry.type == "point") {
-        const graphicLayer = new Graphic(geometry, clickedFeature);
-        view.graphics.removeAll();
-        view.graphics.add(graphicLayer);
-      }
-    });
+      )
+      .catch((error) => {
+        console.error(error);
+      });
   }, []);
 
   return (
--- v2/frontend/components/MarkerSymbol.tsx
+++ v2/frontend/components/MarkerSymbol.tsx
@@ -1,34 +1,31 @@
-import SimpleRenderer from "esri/renderers/SimpleRenderer";
-import SimpleMarkerSymbol from "esri/symbols/SimpleMarkerSymbol";
-
 // If building is not yet clicked, it will be orange with a white outline 
-const simpleMarker = new SimpleMarkerSymbol({
-  style: "circle", 
-  size: "18px",
-  color: "#C64D2A", 
-  outline: {
-    color: "white", 
-    width: 4
+const openFeature = {
+  "type": "simple",
+  "symbol": {
+    "type": "simple-marker",
+    "style": "circle",
+    "size": "18px",
+    "color": "#C64D2A", 
+    "outline": {  
+      "color": "white",
+      "width": 4
+    }
   }
-});
-
-const openFeature = new SimpleRenderer({
-  symbol: simpleMarker
-});
+};
 
 // If building is clicked, it will be black with a white outline
-const simpleMarker2 = new SimpleMarkerSymbol({
-  style: "circle", 
-  size: "18px",
-  color: "black", 
-  outline: {
-    color: "white", 
-    width: 4
+const clickedFeature = {
+  "type": "simple",
+  "symbol": {
+    "type": "simple-marker",
+    "style": "circle",
+    "size": "18px",
+    "color": "black", 
+    "outline": {  
+      "color": "white",
+      "width": 4
+    }
   }
-});
-
-const clickedFeature = new SimpleRenderer({
-  symbol: simpleMarker2
-});
+};
 
 export { clickedFeature,openFeature }
