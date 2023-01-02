731074f20fc7edec99f4b150fa397b28c5dd8257
Franco Reyes <franco.javier.reyes@gmail.com>
2023-01-03 00:13:19 +1100

    Use same database path for prod

--- v2/backend/src/config.ts
+++ v2/backend/src/config.ts
@@ -1,4 +1,4 @@
 const PROD = process.env.NODE_ENV === "production";
 
 export const SCRAPER_PATH = PROD ? "./dist/scraper.js" : "./src/scraper.ts";
-export const DATABASE_PATH = PROD ? "./app/database.json" : "./database.json";
+export const DATABASE_PATH = PROD ? "./database.json" : "./database.json";
