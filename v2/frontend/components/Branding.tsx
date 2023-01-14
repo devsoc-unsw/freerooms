0e7d3619c6b7a4e9e4fcdb0aa6797803715871a6
3bobchen <83627389+3bobchen@users.noreply.github.com>
2023-01-15 10:30:05 +1100

    delete unused imports - Branding.tsx

--- v2/frontend/components/Branding.tsx
+++ v2/frontend/components/Branding.tsx
@@ -1,6 +1,5 @@
 import Box, { BoxProps } from "@mui/material/Box";
-import { orange } from "@mui/material/colors";
-import { styled, SxProps, Theme } from "@mui/material/styles";
+import { styled } from "@mui/material/styles";
 import Typography, { TypographyProps } from "@mui/material/Typography";
 import Image from "next/image";
 
