5e1422b20000c6fc139f4498d0290b194770032a
Urja Arora <urjaarora@Urjas-MacBook-Air.local>
2023-02-27 18:26:53 +1100

    feat(appdirectory): enabled experimental app directory

--- v2/frontend/next.config.js
+++ v2/frontend/next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  output: "standalone"
+  experimental: {
+    appDir: true,
+  },
 };
 
 module.exports = nextConfig;
