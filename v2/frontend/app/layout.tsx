86e426ef91a4a46914465cf1b7b9b261b970fba5
Urja Arora <urjaarora5@gmail.com>
2023-03-13 13:48:39 +1100

    feat(layout): migrated _app.tsx and _document.tsx to layout.tsx)

--- v2/frontend/app/layout.tsx
+++ v2/frontend/app/layout.tsx
@@ -1,3 +1,5 @@
+import "../styles/globals.css";
+
 export default function RootLayout({
   // Layouts must accept a children prop.
   // This will be populated with nested layouts or pages
@@ -7,7 +9,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+        <body>{children}</body>
     </html>
   );
 }
