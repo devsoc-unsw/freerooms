f41503361de3478ef77dde71a7d3c6627e651645
3bobchen <83627389+3bobchen@users.noreply.github.com>
2023-04-07 19:48:43 +1000

    feat(landing): change landing page buttons

--- v2/frontend/components/Landing.tsx
+++ v2/frontend/components/Landing.tsx
@@ -1,12 +1,14 @@
-import Image from "next/image";
-import { useRouter, useSearchParams } from "next/navigation";
-import React, { useEffect } from "react";
+import "../styles/button.css";
+
 import Button, { ButtonProps } from "@mui/material/Button";
 import { styled } from "@mui/material/styles";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import React from "react";
 
-import theme from "./Theme";
 import parentLogo from "../public/assets/favicon/csesocgreyblue.png";
 import Logo from "../public/assets/favicon/free_rooms_logo.png";
+import theme from "./Theme";
 
 const StartBrowsingButton = styled(Button)<ButtonProps>(() => ({
   backgroundColor: "#111",
@@ -48,17 +50,27 @@ const Landing = () => {
           Freerooms
         </h1>
         <p id={"FreeroomsBlurb"} style={{ textAlign: "left", fontWeight: "700" }}>
-          Check UNSW room bookings
+          UNSW room bookings
         </p>
-        <StartBrowsingButton onClick={() => router.push('/gallery')} id={"FreeroomsCTA"}>
-          <p style={{ fontWeight: "600" }}>Start Browsing!</p>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor" className="w-2 h-2">
-              <path strokeLinecap="round" strokeLinejoin="round"
-                    d="M9 12.75l3 3m0 0l3-3m-3 3v-7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+        <div style={{ display: "flex", flexDirection: "column", gap: "1rem" }}>
+          <a className={"button-gradient"} style={{ gap: "1rem", width: "4rem" }} href={"/browse"} id={"FreeroomsCTA"}>
+            <p style={{ fontWeight: "600" }}>Browse</p>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                 stroke="currentColor" className="w-6 h-6">
+              <path stroke-linecap="round" stroke-linejoin="round"
+                    d="M15.75 15.75l-2.489-2.489m0 0a3.375 3.375 0 10-4.773-4.773 3.375 3.375 0 004.774 4.774zM21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
             </svg>
-        </StartBrowsingButton>
+          </a>
+          <a className={"button-gradient"} style={{ gap: "1rem", width: "4rem" }} href={"/map"} id={"FreeroomsCTA"}>
+            <p style={{ fontWeight: "600" }}>Map</p>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                 stroke="currentColor" className="w-6 h-6">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z" />
+              <path stroke-linecap="round" stroke-linejoin="round"
+                    d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z" />
+            </svg>
+          </a>
+        </div>
       </div>
       <div id={"landing-freerooms-logo"} style={{ width: "auto", margin: "auto" }}>
         <Image width={400} height={400} alt={"Freerooms Logo"} src={Logo} />
