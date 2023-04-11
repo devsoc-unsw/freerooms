2065fbaf38b45f2825da9ddc800ff15db4c81f6a
Alec <alectan@hotmail.com>
2023-04-11 14:54:31 +1000

    moved BE fetching to room page

--- v2/frontend/app/room/[room]/page.tsx
+++ v2/frontend/app/room/[room]/page.tsx
@@ -5,26 +5,82 @@ import BookingCalendar from "../../../components/BookingCalendar";
 import Container from "@mui/material/Container";
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
+import type  { ClassList, Class, RoomAvailability } from '../../../types';
+import { API_URL } from "../../../config";
+import { DateTime, Settings } from 'luxon';
 
 
+type Event = {
+	title: string;
+	start: Date,
+	end: Date,
+}
+
+type RoomDetails = {
+	name : string;
+	bookings: Array<Event>
+}
 
 
 export default function Page({ params }: {
   params: {room: string};
 }) {	
+
+	const [ events, setEvents ] = React.useState<Array<Event>>([]);
+	const [ roomName, setRoomName ] = React.useState<string>("");
+	const handleRoomDetails = ({ name, bookings } : RoomDetails ) => {
+		setRoomName(name);
+		setEvents(bookings);
+	}
+
+	React.useEffect( () => {
+		const fetchRoomBookings = () => {
+			fetch( `${API_URL}/rooms/${params.room}`)
+			.then( res => res.json() )
+			.then( json => extractBookings(json))
+			.then( allBookings => handleRoomDetails(allBookings))
+			.catch(() => setEvents([]));
+		}
+		
+		fetchRoomBookings();
+		
+	}, []);
+
   return (
   
     <Container maxWidth={false}>
       <Box sx={{ width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column', paddingTop: 15}}>
         <Typography variant='h4' component='h2'>
-          { `Bookings for ${params.room}`}
+          { `Bookings for ${roomName}`}
         </Typography>
 	      <Box sx={{ width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-					<BookingCalendar roomID={params.room} />
+					<BookingCalendar events={events} />
 	      </Box>
       </Box>
       
     </Container>
     
   );
+}
+
+const extractBookings = ( bookings : RoomAvailability ) => {
+	
+	let allBookings : Array<Event> = [];
+	for( let week = 1; week < 10; ++week) {
+		const bookingsForCurrWeek = bookings[week];
+		Object.keys(bookingsForCurrWeek).forEach(( day ) => {
+			const bookingForCurrDay = bookingsForCurrWeek[day];
+			bookingForCurrDay.forEach((booking) => {
+				allBookings.push({
+					title: booking.courseCode,
+					start: DateTime.fromISO(booking.start).toJSDate(),
+					end: DateTime.fromISO(booking.end).toJSDate(),
+				})
+			});
+		})
+	}
+	
+	const roomDetails : RoomDetails = { name : bookings.name, bookings: allBookings };
+	
+	return roomDetails;
 }
