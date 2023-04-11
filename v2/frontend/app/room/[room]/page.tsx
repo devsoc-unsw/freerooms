6476221eb9e774a4481ddef6c01893bd37b3a7c3
Alec Tan <31383469+alecntan@users.noreply.github.com>
2023-04-11 19:54:36 +1000

    feat(calendar) : Preliminary version of room bookings page (#291)
    * installed Big-Calendar component and @types/big-calender for TS support
    
    * added calendar component. Still need to fix typing
    
    * create component for BigCalendar
    
    * upgraded @types/react to latest (v18)
    
    * created working calendar component that fetches bookings from BE API
    
    * moved BE fetching to room page
    
    * added link to room page
    
    * added check for non-existent keys on room bookings object
    
    * added date picker
    
    * added circular progress component
    
    * removed comment
    
    * feat(build): fix build issues
    
    * feat(build): delete pnpm lock file
    
    * feat(build): new pnpm lock file
    
    * feat(build): suppress ts issues
    
    ---------
    
    Co-authored-by: 3bobchen <83627389+3bobchen@users.noreply.github.com>

--- v2/frontend/app/room/[room]/page.tsx
+++ v2/frontend/app/room/[room]/page.tsx
@@ -1,5 +1,99 @@
-export default async function Page({ params }: {
+"use client" 
+
+import { CircularProgress } from '@mui/material';
+import Box from '@mui/material/Box';
+import Container from "@mui/material/Container";
+import Typography from '@mui/material/Typography';
+import { DateTime } from 'luxon';
+import React from 'react';
+
+import BookingCalendar from "../../../components/BookingCalendar";
+import { API_URL } from "../../../config";
+import type  {  RoomAvailability } from '../../../types';
+
+
+type Event = {
+	title: string;
+	start: Date,
+	end: Date,
+}
+
+type RoomDetails = {
+	name : string;
+	bookings: Array<Event>
+}
+
+export default function Page({ params }: {
   params: {room: string};
-}) {
-  return <h1 style={{marginTop: "6rem"}}>{JSON.stringify(params)}</h1>;
+}) {	
+
+	const [ events, setEvents ] = React.useState<Array<Event>>([]);
+	const [ roomName, setRoomName ] = React.useState<string>("");
+	const handleRoomDetails = ({ name, bookings } : RoomDetails ) => {
+		setRoomName(name);
+		setEvents(bookings);
+	}
+	
+	const handleError = () => {
+		setEvents([]);
+	}
+
+	React.useEffect( () => {
+		const fetchRoomBookings = () => {
+			fetch( `${API_URL}/rooms/${params.room}`)
+			.then( res => res.json() )
+			.then( json => extractBookings(json))
+			.then( allBookings => handleRoomDetails(allBookings))
+			.catch(() => handleError());
+		}
+		
+		fetchRoomBookings();
+		
+	}, []);
+
+  return (
+    
+    <Container maxWidth={false}>
+      { roomName == "" ? (
+        <Box sx={{ width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column', paddingTop: 15, color: 'orange'}}>
+          <CircularProgress color={"inherit"} />
+        </Box>
+      ) : (
+        <Box sx={{ width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column', paddingTop: 15}}>
+	        <Typography variant='h4' component='h2'>
+	          { `Bookings for ${roomName}`}
+	        </Typography>
+		      <Box sx={{ width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+						<BookingCalendar events={events} />
+		      </Box>
+      </Box>
+      )}
+    </Container>
+    
+  );
+}
+
+const extractBookings = ( bookings : RoomAvailability ) => {
+	
+	let allBookings : Array<Event> = [];
+	for( let week = 1; week <= 10; ++week) {
+		if(!( week in bookings ) ) {
+			continue;
+		}
+		const bookingsForCurrWeek = bookings[week];
+		Object.keys(bookingsForCurrWeek).forEach(( day ) => {
+			const bookingForCurrDay = bookingsForCurrWeek[day];
+			bookingForCurrDay.forEach((booking) => {
+				allBookings.push({
+					title: booking.courseCode,
+					start: DateTime.fromISO(booking.start).toJSDate(),
+					end: DateTime.fromISO(booking.end).toJSDate(),
+				})
+			});
+		})
+	}
+	
+	const roomDetails : RoomDetails = { name : bookings.name, bookings: allBookings };
+	
+	return roomDetails;
 }
