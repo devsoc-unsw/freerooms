126b21b5b71e8d6b0cf8b322616024aac61fe830
Franco Reyes <franco.javier.reyes@gmail.com>
2023-04-27 10:14:04 +1000

    Override the locations when scraping

--- v2/backend/src/config.ts
+++ v2/backend/src/config.ts
@@ -2,4 +2,5 @@ const PROD = process.env.NODE_ENV === "production";
 
 export const SCRAPER_PATH = PROD ? "./dist/scraper.js" : "./src/scraper.ts";
 export const DATABASE_PATH = "./database.json";
+export const BLDG_LOCATION_PATH = "./buildingLocations.json";
 export const PORT = 3000;
