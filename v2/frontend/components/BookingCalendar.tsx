8c23311b75e1bbb6f535f8004f53565e44319e7f
Alec <alectan@hotmail.com>
2023-06-29 09:31:28 +1000

    created custom toolbar component w/ mui styling

--- v2/frontend/components/BookingCalendar.tsx
+++ v2/frontend/components/BookingCalendar.tsx
@@ -1,17 +1,24 @@
 'use client'
 
-import "../styles/calendar/css/react-big-calendar.css";
+//import "../styles/calendar/css/react-big-calendar.css";
+import 'react-big-calendar/lib/css/react-big-calendar.css';
 
+import { styled } from '@mui/material/styles';
 import { Typography } from '@mui/material';
-import Box from '@mui/material/Box';
+import ToggleButton, { ToggleButtonProps } from '@mui/material/ToggleButton';
+import Button, { ButtonProps } from "@mui/material/Button";
+import Box, { BoxProps } from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
 import TextField, { TextFieldProps } from "@mui/material/TextField";
 import { DatePicker, LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 import debounce from 'lodash.debounce';
 import React from 'react';
 import { Calendar, dateFnsLocalizer, Views } from 'react-big-calendar';
+import type { View } from 'react-big-calendar';
 import { format, parse, startOfWeek, getDay } from "date-fns";
 import { enAU } from 'date-fns/locale';
+import type { ToolbarProps, NavigateAction } from 'react-big-calendar';
 
 import { selectDatetime } from "../redux/datetimeSlice";
 import {  useSelector } from "../redux/hooks";
@@ -21,8 +28,6 @@ type Event = {
 	start: Date,
 	end: Date,
 }
-
-type ViewTypes = "month" | "week" | "day" | "work_week" | "agenda"; 
 
 const customDatePickerComponent = (
 	params: JSX.IntrinsicAttributes & TextFieldProps,
@@ -37,6 +42,100 @@ const customDatePickerComponent = (
 	/>
 );
 
+const ToolBarContainer = styled(Box)<BoxProps>(({theme}) => ({
+	display: 'flex',
+	flexDirection: 'row',
+	justifyContent: 'space-between',
+	alignContent: 'center',
+	[theme.breakpoints.down('md')]: {
+		display: 'none',
+	},
+}))
+
+const ToolBarButton = styled(Button)<ButtonProps>(( {theme} ) => ({
+	borderRadius: '5px',
+	border: "1px solid",
+	borderColor: 'lightgray',
+	color: "black",
+	size: 'small',
+	fontSize: '12px',
+	marginBottom: '10px',
+	textTransform: 'capitalize',
+	"&:hover": {
+		backgroundColor: theme.palette.primary.main,
+		borderColor: theme.palette.primary.main,
+		color: "#fff",
+	  },
+	
+}))
+
+const ViewToggleButton = styled(ToggleButton)<ToggleButtonProps>(( {theme} ) => ({
+	marginBottom: '10px',
+	textTransform: 'capitalize',
+	fontSize: '12px',
+	paddingTop: '5px',
+	paddingBottom: '5px',
+	width: '65px',
+	borderRadius: '5px',
+	border: "1px solid",
+	borderColor: 'lightgray',
+	"&:hover": {
+		backgroundColor:theme.palette.primary.main,
+		borderColor: theme.palette.primary.main,
+		color: "#fff",
+	},
+	"&.Mui-selected": {
+		backgroundColor: theme.palette.primary.main,
+		borderColor: theme.palette.primary.main,
+		color: "#fff",
+	},
+	"&.Mui-selected:hover" : {
+		backgroundColor: theme.palette.primary.main,
+		borderColor: theme.palette.primary.main,
+		color: "#fff",
+	}
+}))
+
+
+const CustomToolBar : React.FC<ToolbarProps> = ({ date, view, onNavigate, onView }) => {
+
+	const navigationControls : { [ index : string ] : NavigateAction } = {
+		"Previous" : "PREV",
+		"Today" : "TODAY",
+		"Next" : "NEXT" ,
+	};
+
+	const viewControls : { [ index : string ] : View } = {
+		"Month" : "month",
+		"Week" : "week",
+		"Day" : "day", 
+		"Agenda" : "agenda"
+	}
+
+	return (
+		<ToolBarContainer>
+			<Stack
+				direction='row'
+				spacing={1}
+				useFlexGap
+			>
+				{ Object.entries(navigationControls).map(([displayText, navKey], index ) => <ToolBarButton key={index} onClick={() => onNavigate(navKey)}>{displayText}</ToolBarButton>) }
+			</Stack>
+			
+			<Typography sx={{ align: 'center', display: 'flex', alignItems: 'center', fontSize: '16px',  }}>{date.toDateString()}</Typography>
+			<Stack
+				direction='row'
+				spacing={1}
+				useFlexGap
+				sx={{
+				}}
+			>
+				{ Object.entries(viewControls).map(([displayText, viewKey], index ) => <ViewToggleButton value={viewKey} key={index} selected={view == viewKey} onClick={() => onView(viewKey)}>{displayText}</ViewToggleButton>) }
+			</Stack>
+		</ToolBarContainer>
+	)
+}
+
 
 const BookingCalendar : React.FC<{ events : Array<Event> }>= ({ events }) => {
 
@@ -44,7 +143,7 @@ const BookingCalendar : React.FC<{ events : Array<Event> }>= ({ events }) => {
 
 
 	const WINDOWBREAKPOINT = 900;
-	const [ currView, setCurrView ] = React.useState<ViewTypes>(Views.WEEK)
+	const [ currView, setCurrView ] = React.useState<View>(Views.WEEK)
 	
 
 	React.useEffect(() => {
@@ -69,9 +168,11 @@ const BookingCalendar : React.FC<{ events : Array<Event> }>= ({ events }) => {
 
 	}, []);
 
-	const { defaultDate, getNow, localizer, myEvents, scrollToTime } = React.useMemo( () => {
+	const { components, getNow, localizer, myEvents, scrollToTime } = React.useMemo( () => {
 		return {
-			defaultDate: new Date(),
+			components: {
+				toolbar: CustomToolBar, 
+			},
 			getNow: () => new Date(),
 			localizer: dateFnsLocalizer({ format, parse, startOfWeek, getDay, locales: enAU }),
 			myEvents: events,
@@ -108,6 +209,7 @@ const BookingCalendar : React.FC<{ events : Array<Event> }>= ({ events }) => {
 					</Box>
 				</LocalizationProvider>
 				<Calendar
+					components={components}
 					dayLayoutAlgorithm={'no-overlap'}
 					date={date}
 					onNavigate={(newDate) => handleDateChange(newDate)}
