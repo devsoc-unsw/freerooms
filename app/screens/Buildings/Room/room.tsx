ed0a8d889f27f3e638875fe6e1abfb339dfe1bc7
--replace-all <--replace-all>
2023-07-03 15:07:02 +1000

    started on room page

--- app/screens/Buildings/Room/room.tsx
+++ app/screens/Buildings/Room/room.tsx
@@ -4,21 +4,56 @@ import {
 	View,
 } from "react-native";
 
+
 import type { BuildingStackScreenProps } from "../../types";
+import { TabRouter } from "@react-navigation/native";
 
 export default function Room({ route, navigation } : BuildingStackScreenProps<"Room"> ) {
+	console.log(route);
 	return (
 		<View style={ styles.container }>
-			<Text> Invidividual Room Page </Text>
+			<Text style={ styles.main_heading }>{route.params.roomName}</Text>
+			<Text style={styles.avaliable}> {route.params.status == "free" ? "Available" : "Unvailable"} </Text>
+			<Elements></Elements>
+
 		</View>
 	);
 }
 
+const Elements = () => {
+	return (
+
+			<View>
+				<View style={styles.column} >
+				<Text>Capacity</Text> 
+			</View>
+			<View style={styles.column}>
+				<Text>Room Type</Text>
+			</View>
+
+
+		</View>
+
+	)
+}
+
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  main_heading: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    paddingBottom: 20,
+  },
+  avaliable: {
+	fontSize: 18,
+	paddingBottom: 20
+  },
+  column: {
+	width: '50%'
+
   },
 });
