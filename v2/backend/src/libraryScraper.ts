bee149aed866e239173bb11f89f3d565aea5e976
Harry Zhang <z5363493@ad.unsw.edu.au>
2023-07-14 17:02:56 +1000

    feat(scraper): downloaded webpage

--- v2/backend/src/libraryScraper.ts
+++ v2/backend/src/libraryScraper.ts
@@ -2,5 +2,18 @@ import { load, SelectorType } from 'cheerio';
 
 import { RoomBooking, Room } from "./types";
 
+import axios from "axios";
+
 const LIBRARY_BOOKINGS_URL = "https://unswlibrary-bookings.libcal.com/";
 
+
+const location = 6581;
+const zone = 0;
+const category = 0;
+const capacity = 0;
+
+const url = `https://unswlibrary-bookings.libcal.com/r/new/availability?lid=${location}&zone=${zone}&gid=${category}&capacity=${capacity}`
+
+const response = axios.get(url);
+
+console.log(response);
