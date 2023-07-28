e51671975dd8b8b5e29d964da69359a0ec83ff3e
Sean Wibisono <wibiseano@gmail.com>
2023-07-28 17:12:58 +1000

    puppeteer

--- v2/backend/src/libraryScraper.ts
+++ v2/backend/src/libraryScraper.ts
@@ -1,4 +1,5 @@
 import { load, SelectorType } from 'cheerio';
+import { launch } from 'puppeteer';
 
 import { RoomBooking, Room } from "./types";
 
@@ -7,19 +8,45 @@ import axios from "axios";
 const LIBRARY_BOOKINGS_URL = "https://unswlibrary-bookings.libcal.com/";
 
 const downloadPage = async() => {
+    
     const location = 6581;
     const zone = 0;
     const category = 0;
     const capacity = 0;
 
-    const url = `https://unswlibrary-bookings.libcal.com/r/new/availability?lid=${location}&zone=${zone}&gid=${category}&capacity=${capacity}`
+    const url = `${LIBRARY_BOOKINGS_URL}r/new/availability?lid=${location}&zone=${zone}&gid=${category}&capacity=${capacity}`
 
     return axios.get(url);
 }
 
 const getRooms = async() => {
     const response = await downloadPage();
-    console.log(response);
+    // console.log(response.data);
+    const $ = load(response.data);
+    console.log($('table'));
 }
 
-getRooms();
+const scrape = async() => {
+    const browser = await launch();
+    const page = await browser.newPage();
+
+    const location = 6581;
+    const zone = 0;
+    const category = 0;
+    const capacity = 0;
+
+    const url = `${LIBRARY_BOOKINGS_URL}r/new/availability?lid=${location}&zone=${zone}&gid=${category}&capacity=${capacity}`
+
+    await page.goto(url, {
+        waitUntil: 'networkidle2',
+    });
+
+    const content = await page.content();
+    const $ = load(content);
+    console.log($('table'))
+
+    await browser.close();
+}
+
+// getRooms();
+scrape();
