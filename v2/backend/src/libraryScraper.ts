122bf73998955d923f347c4c216bcf685e9249ad
Sean Wibisono <wibiseano@gmail.com>
2023-09-05 16:24:23 +1000

    Found booking data endpoint

--- v2/backend/src/libraryScraper.ts
+++ v2/backend/src/libraryScraper.ts
@@ -1,51 +1,59 @@
 import { load, SelectorType } from 'cheerio';
-import { launch } from 'puppeteer';
 
 import { RoomBooking, Room } from "./types";
 
 import axios from "axios";
+import fs from "fs";
 
 const LIBRARY_BOOKINGS_URL = "https://unswlibrary-bookings.libcal.com/";
 
+const POST_URL = "https://unswlibrary-bookings.libcal.com/spaces/availability/grid";
+
 const downloadPage = async() => {
-    
-    const location = 6581;
-    const zone = 0;
-    const category = 0;
-    const capacity = 0;
 
-    const url = `${LIBRARY_BOOKINGS_URL}r/new/availability?lid=${location}&zone=${zone}&gid=${category}&capacity=${capacity}`
+    const postData = {
+        lid: '6581',
+        gid: '0',
+        eid: '-1',
+        seat: '0',
+        seatId: '0',
+        zone: '0',
+        start: '2023-09-05',
+        end: '2023-09-06',
+        pageIndex: '0',
+        pageSize: '18'
+    };
 
-    return axios.get(url);
-}
+    const headers = {
+        'Content-Type': 'application/x-www-form-urlencoded', // because the request data is URL encoded
+        'Referer': 'https://unswlibrary-bookings.libcal.com/r/new/availability?lid=6581&zone=0&gid=0&capacity=-1'
+    };
 
-const getRooms = async() => {
-    const response = await downloadPage();
-    // console.log(response.data);
-    const $ = load(response.data);
-    console.log($('table'));
+    try {
+        const response = await axios.post(POST_URL, new URLSearchParams(postData), {headers: headers});
+        const responseString = JSON.stringify(response.data, null, 4);
+        fs.writeFile('out.txt', responseString, (err) => {
+            if (err) throw err;
+        })
+
+        return response;
+    } catch (error) {
+        console.error("Error fetching availability:", error);
+    }
 }
 
 const scrape = async() => {
-    const browser = await launch();
-    const page = await browser.newPage();
 
-    const location = 6581;
-    const zone = 0;
-    const category = 0;
-    const capacity = 0;
+    // const browser = await launch();
 
-    const url = `${LIBRARY_BOOKINGS_URL}r/new/availability?lid=${location}&zone=${zone}&gid=${category}&capacity=${capacity}`
+    const response = await downloadPage();
+    // console.log(response.data);
+    // const $ = load(response);
+    
+    // const $ = load(response);
+    // console.log($('table.fc-scrollgrid'));
 
-    await page.goto(url, {
-        waitUntil: 'networkidle2',
-    });
 
-    const content = await page.content();
-    const $ = load(content);
-    console.log($('table'))
-
-    await browser.close();
 }
 
 // getRooms();
