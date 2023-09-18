5e0c0b5127c827be31e91f5ffc7c1203ad5ba385
Franco Reyes <franco.javier.reyes@gmail.com>
2023-09-19 01:43:04 +1000

    Use building aliases

--- frontend/components/SearchModal.tsx
+++ frontend/components/SearchModal.tsx
@@ -43,7 +43,7 @@ const SearchModal: React.FC<SearchProps> = ({ open, setOpen }) => {
     const buildingOptions: BuildingSearchOption[] = buildings
       ? buildings.map(building => ({
         type: "Building",
-        searchKeys: [building.name, building.id],
+        searchKeys: [building.name, ...building.aliases, building.id],
         building
       }))
       : [];
