d0d5c18847ee94bde49257eab8a7283affbb7fa1
Franco Reyes <franco.javier.reyes@gmail.com>
2023-11-15 15:03:53 +1100

    fix: import fonts

--- frontend/app/clientLayout.tsx
+++ frontend/app/clientLayout.tsx
@@ -1,5 +1,10 @@
 "use client"
 
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 import { orange } from "@mui/material/colors";
 import CssBaseline from "@mui/material/CssBaseline";
 import { createTheme, styled } from "@mui/material/styles";
