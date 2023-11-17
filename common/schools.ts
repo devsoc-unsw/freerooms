17850982aa3e598175931011aeacb393057a0e3b
Alec <alectan@hotmail.com>
2023-11-17 11:34:09 +1100

    Added school to details mapping

---
+++ common/schools.ts
@@ -0,0 +1,149 @@
+const schools = {
+    "ARTSC": {
+        name: "Faculty of Arts, Design & Architecture",
+        homepage: "https://www.unsw.edu.au/arts-design-architecture",
+        contactLink: "https://www.unsw.edu.au/arts-design-architecture/about-us/contact-us"
+    },
+    "BABS": {
+        name: "School of Biotechnology & Biomolecular Sciences",
+        homepage: "https://www.unsw.edu.au/science/our-schools/babs",
+        contactLink: "https://www.unsw.edu.au/science/our-schools/babs/about-us/contact-us"
+    },
+    "BEES": {
+        name: "School of Biological, Earth & Environmental Sciences",
+        homepage: "https://www.unsw.edu.au/science/our-schools/bees",
+        contactLink: "https://www.unsw.edu.au/science/our-schools/bees/contact-us"
+    },
+    "BLTEN": {
+        name: "School of Built Environment",
+        homepage: "https://www.unsw.edu.au/arts-design-architecture/our-schools/built-environment",
+        contactLink: "https://www.unsw.edu.au/arts-design-architecture/our-schools/built-environment/about-us/contact-us"
+    },
+    "BLTENS": {
+        name: "School of Built Environment",
+        homepage: "https://www.unsw.edu.au/arts-design-architecture/our-schools/built-environment",
+        contactLink: "https://www.unsw.edu.au/arts-design-architecture/our-schools/built-environment/about-us/contact-us"
+    },
+    "CEIC": {
+        name: "School of Chemical Engineering",
+        homepage: "https://www.unsw.edu.au/engineering/our-schools/chemical-engineering",
+        contactLink: "https://www.unsw.edu.au/engineering/our-schools/chemical-engineering/about-us/contact-us"
+    },
+    "CHEM": {
+        name: "School of Chemical Engineering",
+        homepage: "https://www.unsw.edu.au/engineering/our-schools/chemical-engineering",
+        contactLink: "https://www.unsw.edu.au/engineering/our-schools/chemical-engineering/about-us/contact-us"
+    },
+    "COMM": {
+        name: "Business School",
+        homepage: "https://www.unsw.edu.au/business",
+        contactLink: "https://www.unsw.edu.au/business/about-us/contact-us"
+    },
+    "COMPSC": {
+        name: "School of Computer Science and Engineering",
+        homepage: "https://www.unsw.edu.au/engineering/our-schools/computer-science-and-engineering",
+        contactLink: "https://www.unsw.edu.au/engineering/our-schools/computer-science-and-engineering/about-us/contact-us"
+    },
+    "CVEN": {
+        name: "School of Civil and Environmental Engineering",
+        homepage: "https://www.unsw.edu.au/engineering/our-schools/civil-and-environmental-engineering",
+        contactLink: "https://www.unsw.edu.au/engineering/our-schools/civil-and-environmental-engineering"
+    },
+    "EDUC": {
+        name: "School of Education",
+        homepage: "https://www.unsw.edu.au/arts-design-architecture/our-schools/education/",
+        contactLink: "https://www.unsw.edu.au/arts-design-architecture/our-schools/education/about-us/contact-us"
+    },
+    "ELEC": {
+        name: "School of Electrical Engineering and Telecommunications",
+        homepage: "https://www.unsw.edu.au/engineering/our-schools/electrical-engineering-telecommunications",
+        contactLink: "https://www.unsw.edu.au/engineering/our-schools/electrical-engineering-telecommunications/about-us/contact-us"
+    },
+    "ENG": {
+        name: "Faculty of Engineering",
+        homepage: "https://www.unsw.edu.au/engineering",
+        contactLink: "https://www.unsw.edu.au/engineering/about-us/contact-us"
+    },
+    "ENGL": {
+        name: "School of Arts and Media",
+        homepage: "https://www.unsw.edu.au/arts-design-architecture/our-schools/arts-media",
+        contactLink: "https://www.unsw.edu.au/arts-design-architecture/our-schools/arts-media/about-us/contact-us"
+    },
+    "GBIOM": {
+        name: "Graduate School of Biomedical Engineering",
+        homepage: "https://www.unsw.edu.au/engineering/our-schools/biomedical-engineering",
+        contactLink: "https://www.unsw.edu.au/engineering/our-schools/biomedical-engineering/about-us/contact-us"
+    },
+    "GLOBL": {
+        name: "UNSW College",
+        homepage: "https://www.unswcollege.edu.au/",
+        contactLink: "https://www.unswcollege.edu.au/contact-us"
+    },
+    "HUMSLANG": {
+        name: "School of Humanities & Languages",
+        homepage: "https://www.unsw.edu.au/arts-design-architecture/our-schools/humanities-languages",
+        contactLink: "https://www.unsw.edu.au/arts-design-architecture/our-schools/humanities-languages/about-us/contact-us"
+    },
+    "LAW": {
+        name: "Law & Justice Faculty",
+        homepage: "https://www.unsw.edu.au/law-justice",
+        contactLink: "https://www.unsw.edu.au/law-justice/about-us/contact-us"
+    },
+    "MATH": {
+        name: "School of Mathematics and Statistics",
+        homepage: "https://www.unsw.edu.au/science/our-schools/maths",
+        contactLink: "https://www.unsw.edu.au/science/our-schools/maths/contact"
+    },
+    "MATSCEN": {
+        name: "School of Materials Science and Engineering",
+        homepage: "https://www.unsw.edu.au/science/our-schools/materials",
+        contactLink: "https://www.unsw.edu.au/science/our-schools/materials/about-us/contact-us"
+    },
+    "MECH": {
+        name: "School of Mechanical and Manufacturing Engineering",
+        homepage: "https://www.unsw.edu.au/engineering/our-schools/mechanical-and-manufacturing-engineering",
+        contactLink: "https://www.unsw.edu.au/engineering/our-schools/mechanical-and-manufacturing-engineering/about-us/contact-us"
+    },
+    "MED": {
+        name: "Medicine & Health Faculty",
+        homepage: "https://www.unsw.edu.au/medicine-health",
+        contactLink: "https://www.unsw.edu.au/medicine-health/about-us/contact-us"
+    },
+    "MEDIA": {
+        name: "School of Arts and Media",
+        homepage: "https://www.unsw.edu.au/arts-design-architecture/our-schools/arts-media",
+        contactLink: "https://www.unsw.edu.au/arts-design-architecture/our-schools/arts-media/about-us/contact-us"
+    },
+    "MUSIC": {
+        name: "School of Arts and Media",
+        homepage: "https://www.unsw.edu.au/arts-design-architecture/our-schools/arts-media",
+        contactLink: "https://www.unsw.edu.au/arts-design-architecture/our-schools/arts-media/about-us/contact-us"
+    },
+    "OPTOM": {
+        name: "School of Optometry & Vision Science",
+        homepage: "https://www.unsw.edu.au/medicine-health/our-schools/optometry",
+        contactLink: "https://www.unsw.edu.au/medicine-health/our-schools/optometry"
+    },
+    "PETRO": {
+        name: "School of Minerals and Energy Resources Engineering",
+        homepage: "https://www.unsw.edu.au/engineering/our-schools/minerals-and-energy-resources-engineering",
+        contactLink: "https://www.unsw.edu.au/engineering/our-schools/minerals-and-energy-resources-engineering/about-us/contact-us"
+    },
+    "POLS": {
+        name: "School of Social Sciences",
+        homepage: "https://www.unsw.edu.au/arts-design-architecture/our-schools/social-sciences",
+        contactLink: "https://www.unsw.edu.au/arts-design-architecture/our-schools/social-sciences"
+    },
+    "PSYC": {
+        name: "School of Psychology",
+        homepage: "https://www.unsw.edu.au/science/our-schools/psychology",
+        contactLink: "https://www.unsw.edu.au/science/our-schools/psychology/about-us/contact-us"
+    },
+    "SISTM": {
+        name: "School of Information Systems & Technology Management",
+        homepage: "https://www.unsw.edu.au/business/our-schools/information-systems-technology-management",
+        contactLink: "https://www.unsw.edu.au/business/our-schools/information-systems-technology-management/contact-us"
+    },
+}
+
+export default schools;
