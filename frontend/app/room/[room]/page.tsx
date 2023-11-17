0d72e8f8b802cb18e2302c0d620f306a825741a8
Franco Reyes <franco.javier.reyes@gmail.com>
2023-11-17 16:25:58 +1100

    Small styling fixes

--- frontend/app/room/[room]/page.tsx
+++ frontend/app/room/[room]/page.tsx
@@ -1,6 +1,10 @@
 "use client" 
 
-import  {Dialog, DialogActions,DialogContent, DialogContentText, DialogTitle} from '@mui/material';
+import getSchoolDetails from "@common/getSchoolDetails";
+import translateUsage from "@common/translateUsage";
+import type { Room } from "@common/types";
+import CloseIcon from "@mui/icons-material/Close";
+import  {Dialog, DialogContent, DialogContentText, DialogTitle, IconButton} from '@mui/material';
 import Box from '@mui/material/Box';
 import Container from "@mui/material/Container";
 import Link from '@mui/material/Link';
@@ -11,9 +15,6 @@ import Image from "next/image";
 import React from 'react';
 import { useState } from 'react';
 
-import getSchoolDetails from "../../../../common/getSchoolDetails";
-import translateUsage from "../../../../common/translateUsage";
-import type { Room } from "../../../../common/types";
 import BookingCalendar from "../../../components/BookingCalendar";
 import Button from "../../../components/Button";
 import LoadingCircle from "../../../components/LoadingCircle";
@@ -65,16 +66,16 @@ const RoomPageHeader : React.FC<{ room : Room, buildingName: string }> = ({
 			alignItems={'center'}
 			justifyContent={"space-between"}
 		>
-			<Stack direction={"column"} spacing={1} width="100%">
-                { buildingName != "" ? (
-                    <Stack direction='row' spacing={2} >
+			<Stack direction={"column"} spacing={1} width="100%" mb={1}>
+				{buildingName != "" && (
+					<Stack direction='row' spacing={2} >
 						<Typography display="inline" variant="subtitle2">{`${buildingName}`} </Typography>
-                        <Typography display="inline" variant="subtitle2">{"/"}</Typography>
-                        <Typography display="inline" variant="subtitle2">{`${translateUsage(room.usage)}`}</Typography>
-                        { room.school != " " ? <Typography display="inline" variant="subtitle2">{"/"}</Typography> : null }
-                        { room.school != " " ? <Typography display="inline" fontWeight={"bold"} color={"#e65100"} variant="subtitle2">{"ID Required"}</Typography> : null }
-                    </Stack>
-                ) : null }
+						<Typography display="inline" variant="subtitle2">{"/"}</Typography>
+						<Typography display="inline" variant="subtitle2">{`${translateUsage(room.usage)}`}</Typography>
+						{ room.school != " " ? <Typography display="inline" variant="subtitle2">{"/"}</Typography> : null }
+						{ room.school != " " ? <Typography display="inline" fontWeight={"bold"} color={"#e65100"} variant="subtitle2">{"ID Required"}</Typography> : null }
+					</Stack>
+				)}
 				<Box display="flex" justifyContent={"space-between"} alignItems={'center'} width={'100%'} >
 					<Typography variant='h4' fontWeight={550}> {room.name} </Typography>
 					<BookingButton school={room.school} usage={room.usage} onClick={toggleDialog} />
@@ -90,31 +91,38 @@ const RoomPageHeader : React.FC<{ room : Room, buildingName: string }> = ({
 						Abbreviation: <Typography display={"inline"} variant="body1">{room.abbr}</Typography>
 					</Typography>
 
-					{ room.school != " " ? (
-                     <Typography variant="body1" fontWeight={"bold"}>
-						School: <Typography display={"inline"} variant="body1">{ schoolDetails ? schoolDetails.name : room.school }</Typography>
-					</Typography>
-                    ) : null }
+					{room.school != " " && (
+					  <Typography variant="body1" fontWeight={"bold"}>
+							School: <Typography display={"inline"} variant="body1">{ schoolDetails ? schoolDetails.name : room.school }</Typography>
+						</Typography>
+					)}
 				</Stack>
 			</Stack>
 			<Dialog open={openDialog} onClose={toggleDialog} PaperProps={{ sx: { borderRadius: '10px' }}}>
 				<DialogTitle>
 					<Typography variant={"h6"} fontWeight={"Bold"}>Booking this Room</Typography>
 				</DialogTitle>
-				<DialogContent>
+				<IconButton
+					aria-label="close"
+					onClick={() => setDialog(false)}
+					sx={{
+						position: 'absolute',
+						right: 12,
+						top: 12,
+						color: (theme) => theme.palette.grey[500],
+					}}
+				>
+					<CloseIcon />
+				</IconButton>
+				<DialogContent dividers>
 					<DialogContentText>
 						<Stack direction={"column"} spacing={2}>
 							<Typography variant={"body1"}>
 								{ dialogMessage }
 							</Typography>
-							{ schoolDetails ? <Typography variant={"body1"}> You can find the contact details of the school <Link href={schoolDetails.contactLink}>here</Link>. </Typography> : null } 
+							{ schoolDetails ? <Typography variant={"body1"}> You can find the contact details of the school <Link target="_blank" href={schoolDetails.contactLink}>here</Link>. </Typography> : null }
 						</Stack>
 					</DialogContentText>
-					<DialogActions>
-						<Button onClick={toggleDialog} sx={{ px: 2, py: 1 }}>
-							<Typography variant={"body2"} fontWeight={"bold"}>Close</Typography> 
-						</Button>
-					</DialogActions>
 				</DialogContent>
 			</Dialog>
 		</Box>
@@ -128,16 +136,18 @@ const BookingButton : React.FC<{ school: string, usage: string, onClick : () => 
     if( school === " " && usage === "LIB" ) link = "https://unswlibrary-bookings.libcal.com";
     else if( school === " " ) link = "https://www.learningenvironments.unsw.edu.au/make-booking/book-room";
 
-    if( link != "" ) return (
-        <Button href={link} sx={{ px: 2, py: 1}}>
-	        <Typography variant={"body2"} fontWeight={"bold"}>Make a Booking</Typography>
-		</Button>
+    if (link != "") return (
+			<Link target="_blank" href={link}>
+				<Button  sx={{ px: 2, py: 1, height: 45 }} >
+					<Typography variant={"body2"} fontWeight={"bold"}>Make a Booking</Typography>
+				</Button>
+			</Link>
     );
 
     return (
-        <Button onClick={onClick} sx={{ px: 2, py: 1}}>
-	        <Typography variant={"body2"} fontWeight={"bold"}>Make a Booking</Typography>
-		</Button>
+			<Button onClick={onClick} sx={{ px: 2, py: 1, height: 45 }}>
+				<Typography variant={"body2"} fontWeight={"bold"}>Make a Booking</Typography>
+			</Button>
     );
 }
 
