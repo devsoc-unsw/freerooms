a3d41a82dde3f51daa38c687233bda6f8f7ed136
Franco Reyes <franco.javier.reyes@gmail.com>
2024-03-26 01:01:38 +1100

    add mapping for @common directory to jest config

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -2,6 +2,8 @@ module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
   roots: ["<rootDir>/"],
-  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  moduleNameMapper: {
+    "@common/(.*)": "<rootDir>/../common/$1",
+  },
 };
