b206e94f52df583f22ae15e95b27e688e8687035
Franco Reyes <franco.javier.reyes@gmail.com>
2024-04-04 18:34:23 +1100

    add render with redux

--- frontend/redux/store.ts
+++ frontend/redux/store.ts
@@ -1,29 +1,38 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 
 import currentBuildingReducer from "./currentBuildingSlice";
 import datetimeReducer from "./datetimeSlice";
 import filtersReducer from "./filtersSlice";
 import searchOpenSlice from "./searchOpenSlice";
 
-const store = configureStore({
-  reducer: {
+// Create the root reducer separately, so we can extract the RootState type
+const rootReducer = combineReducers({
     currentBuilding: currentBuildingReducer,
     datetime: datetimeReducer,
     filters: filtersReducer,
-    searchOpen: searchOpenSlice
-  },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        // Allow Date object in store
-        ignoredActions: ['datetime/setDatetime'],
-        ignoredPaths: ['datetime'],
-      },
-    }),
-})
+    searchOpen: searchOpenSlice,
+});
+
+export const setupStore = (preloadedState?: Partial<RootState>) => {
+  return configureStore({
+    reducer: rootReducer,
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware({
+        serializableCheck: {
+          // Allow Date object in store
+          ignoredActions: ["datetime/setDatetime"],
+          ignoredPaths: ["datetime"],
+        },
+      }),
+    preloadedState
+  })
+}
+
+const store = setupStore();
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
-export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof rootReducer>
+export type AppStore = ReturnType<typeof setupStore>
+export type AppDispatch = AppStore['dispatch']
 
 export default store;
