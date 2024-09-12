2d8584c3f8257cbc8ad5ab1798d9d295bb1bee5c
someguy <andrew.zhang0405@gmail.com>
2024-09-13 00:28:10 +1000

    Creating new DecimalStarRating component which contains the decimal and the star rating of the room to reduce nesting

--- frontend/components/OverallRating.tsx
+++ frontend/components/OverallRating.tsx
@@ -1,9 +1,8 @@
-import { Box, Button, Modal } from "@mui/material";
-import Rating from "@mui/material/Rating";
-import Typography from "@mui/material/Typography";
+import { Button } from "@mui/material";
 import Stack from "@mui/system/Stack";
 import React, { useState } from "react";
 
+import DecimalStarRating from "./DecimalStarRating";
 import LinearRatings from "./LinearRatings";
 import ReviewModal from "./ReviewModal";
 
@@ -37,28 +36,7 @@ const OverallRating = () => {
           height="150%"
           aria-label="Number Star Rating"
         >
-          <Typography
-            component="legend"
-            variant="h3"
-            sx={{
-              marginBottom: "16px",
-              fontSize: {
-                xs: "2.5rem",
-                md: "5rem",
-              },
-              width: "100%",
-              textAlign: "center",
-            }}
-          >
-            {4.5}
-          </Typography>
-          <Rating
-            name="decimal-rating"
-            defaultValue={4.5}
-            precision={0.1}
-            readOnly
-            size="large"
-          />
+          <DecimalStarRating />
           <Button
             onClick={handleOpen}
             sx={{
