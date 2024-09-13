40e3e52890bc1af270983805510f42c8a288797f
cherisechan <cherisechan@outlook.com.au>
2024-09-13 18:01:09 +1000

    changed require to import and added .env

--- backend/src/ratingDbInterface.ts
+++ backend/src/ratingDbInterface.ts
@@ -1,7 +1,15 @@
-const {MongoClient} = require('mongodb');
-const uri = "mongodb+srv://freerooms:Freerooms123-456@freerooms.bceee3g.mongodb.net/?retryWrites=true&w=majority&appName=freerooms"
+import {MongoClient} from 'mongodb'
+
+import dotenv from 'dotenv';
+dotenv.config({ path: 'src/.env.local' });
+
+const uri: string | undefined = process.env.MONGODB_URI;
 
 export async function insertRating(roomId: string, ratings: number[]): Promise<void> {
+    if (!uri) {
+        throw new Error('uri not found')
+    }
+
     const client = new MongoClient(uri);
     try {
         await client.connect();
@@ -20,24 +28,3 @@ export async function insertRating(roomId: string, ratings: number[]): Promise<v
         await client.close();
     }
 }
-
-
-// export async function fetchAverageExperience(roomName: string): Promise<number> {
-//     const client = new MongoClient(uri);
-//     try {
-//         await client.connect();
-//         const database = client.db('room-ratings');
-//         const collection = database.collection('ratings');
-
-//         // Find all documents matching the room name
-//         const query = { roomName: roomName };
-//         const results = await collection.find(query).toArray();
-
-//         return results;
-//     } catch (error) {
-//         console.error("Error fetching ratings:", error);
-//         throw new Error("Failed to fetch ratings");
-//     } finally {
-//         await client.close();
-//     }
-// }
