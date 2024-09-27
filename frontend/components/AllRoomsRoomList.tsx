38a0d1debab126c6e570dd3c6181851418cab8b2
Mark Tran <z5421592@ad.unsw.edu.au>
2024-09-27 18:56:09 +1000

    Limit container of static room blocks to be scrollable and not page scrollable

--- frontend/components/AllRoomsRoomList.tsx
+++ frontend/components/AllRoomsRoomList.tsx
@@ -4,14 +4,16 @@ import { styled } from "@mui/system";
 const RoomList = styled(Stack)(({ theme }) => ({
   flexDirection: "column",
   flexGrow: 5,
+  maxHeight: "calc(100vh - 225px)",
+  overflowY: "auto",
   [theme.breakpoints.down("md")]: {
     width: "100%",
     marginTop: 14,
   },
   [theme.breakpoints.up("md")]: {
     width: "75%",
-    marginLeft: 10,
-    marginTop: 8,
+    marginLeft: 0,
+    marginTop: 0,
   },
 }));
 
