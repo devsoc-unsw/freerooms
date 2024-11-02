fb3988d627bd0f36a85f24e712baee5b9d9ca013
Livia Wang <z5417230@ad.unsw.edu.au>
2024-11-03 00:24:44 +1100

    removed unused imports

--- frontend/__tests__/CircularRating.test.tsx
+++ frontend/__tests__/CircularRating.test.tsx
@@ -2,18 +2,12 @@ import "@testing-library/jest-dom";
 
 import { render, screen } from "@testing-library/react";
 import RoomRatingList from "components/Rooms/RoomRatingList";
-import { useRouter } from "next/navigation";
-import { Provider } from "react-redux";
-
-import Page from "../app/room/[room]/page";
-import store from "../redux/store";
 
 describe("Rooms page with circular rating component", () => {
   it("renders the CircularRating component", () => {
     const mockParams = "K-J17-101";
 
     render(<RoomRatingList roomID={mockParams} />);
-    screen.debug();
 
     const cleanlinessRating = screen.getByText("Cleanliness");
     const quietnessRating = screen.getByText("Quietness");
