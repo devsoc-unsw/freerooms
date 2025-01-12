297eba333aced6db5274016c3dfe4d19578ca8b4
Michael Tanto <michaeltanto0909@gmail.com>
2025-01-12 15:59:54 +1100

    Setup Xcode iOS project

---
+++ ios/Freerooms/Freerooms/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Freerooms
+//
+//  Created by Michael Tanto on 12/01/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ios/Freerooms/Freerooms/FreeroomsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FreeroomsApp.swift
+//  Freerooms
+//
+//  Created by Michael Tanto on 12/01/25.
+//
+
+import SwiftUI
+
+@main
+struct FreeroomsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
