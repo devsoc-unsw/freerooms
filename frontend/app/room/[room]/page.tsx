fa256d4a488e28a4cc4139c9f3477754ce936965
Ben Godwin <104501525+bengodw@users.noreply.github.com>
2025-04-28 12:23:31 +1000

    598 Displaying Room Images (#602)
    * added room images
    
    * lint
    
    ---------
    
    Co-authored-by: Michael Tanto <80404890+mt-fns@users.noreply.github.com>

--- frontend/app/room/[room]/page.tsx
+++ frontend/app/room/[room]/page.tsx
@@ -17,6 +17,7 @@ import Link from "@mui/material/Link";
 import Rating from "@mui/material/Rating";
 import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
+import { Dictionary } from "@reduxjs/toolkit";
 import RoomRating from "components/Rating/RoomRating";
 import useRoomRatings from "hooks/useRoomRatings";
 import Image from "next/image";
@@ -29,6 +30,7 @@ import RoomBackButton from "../../../components/RoomBackButton";
 import useBookings from "../../../hooks/useBookings";
 import useBuilding from "../../../hooks/useBuilding";
 import useRoom from "../../../hooks/useRoom";
+import room_photos from "../../../public/room-photos.json";
 
 const adjustDateIfMidnight = (inputDate: Date): Date => {
   // Check if the time is midnight (00:00:00)
@@ -70,7 +72,13 @@ export default function Page({ params }: { params: { room: string } }) {
           px={{ xs: 3, md: 15 }}
         >
           <RoomPageHeader room={room} buildingName={building.name} />
-          <RoomImage src={`/assets/building_photos/${campus}-${grid}.webp`} />
+          <RoomImage
+            src={
+              params.room in room_photos
+                ? `${(room_photos as Dictionary<String>)[params.room]}`
+                : `/assets/building_photos/${campus}-${grid}.webp`
+            }
+          />
           <BookingCalendar events={adjustedBookings ?? []} roomID={room.id} />
           <RoomRating buildingID={building.id} roomID={room.id} />
         </Stack>
