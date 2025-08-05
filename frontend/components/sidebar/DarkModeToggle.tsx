640f4e15999c06cc948560459a28a3089bc6b00a
Brandon Tan <brandon96tan@gmail.com>
2025-08-06 00:22:27 +1000

    sidebar refactor with theme and proper spacing

--- frontend/components/sidebar/DarkModeToggle.tsx
+++ frontend/components/sidebar/DarkModeToggle.tsx
@@ -1,68 +1,27 @@
 import { DarkModeContext } from "@frontend/app/clientLayout";
-import {
-  ListItem,
-  ListItemButton,
-  ListItemIcon,
-  ListItemText,
-} from "@mui/material";
+import { useTheme } from "@mui/material";
 import { MoonIcon, SunIcon } from "lucide-react";
 import React from "react";
 
+import SidebarItem from "./SidebarItem";
+
 const DarkModeToggle = ({ sidebarOpen }: { sidebarOpen: boolean }) => {
   const { isDarkMode, toggleDarkMode } = React.useContext(DarkModeContext);
+  const theme = useTheme();
 
   return (
-    <ListItem disablePadding sx={{ display: "block", marginBottom: 1 }}>
-      <ListItemButton
-        disableRipple
-        onClick={toggleDarkMode}
-        sx={[
-          {
-            minHeight: 48,
-            px: 2.5,
-            justifyContent: "center",
-            borderRadius: "8px",
-          },
-          sidebarOpen
-            ? {
-                justifyContent: "initial",
-              }
-            : {
-                justifyContent: "center",
-              },
-        ]}
-      >
-        <ListItemIcon
-          sx={[
-            {
-              minWidth: 0,
-              justifyContent: "center",
-            },
-            sidebarOpen
-              ? {
-                  mr: 3,
-                }
-              : {
-                  mr: "auto",
-                },
-          ]}
-        >
-          {isDarkMode ? <MoonIcon /> : <SunIcon />}
-        </ListItemIcon>
-        <ListItemText
-          primary={isDarkMode ? "Light Mode" : "Dark Mode"}
-          sx={[
-            sidebarOpen
-              ? {
-                  opacity: 1,
-                }
-              : {
-                  opacity: 0,
-                },
-          ]}
-        />
-      </ListItemButton>
-    </ListItem>
+    <SidebarItem
+      icon={
+        isDarkMode ? (
+          <MoonIcon size={20} color={theme.palette.text.primary} />
+        ) : (
+          <SunIcon size={20} color={theme.palette.text.primary} />
+        )
+      }
+      label="Dark Mode"
+      onClick={toggleDarkMode}
+      sidebarOpen={sidebarOpen}
+    />
   );
 };
 
--- frontend/components/sidebar/Sidebar.tsx
+++ frontend/components/sidebar/Sidebar.tsx
@@ -1,31 +1,36 @@
-import LogoOpen from "@frontend/public/assets/logo-open.svg";
-import { AppBarProps as MuiAppBarProps } from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import CssBaseline from "@mui/material/CssBaseline";
-import Divider from "@mui/material/Divider";
-import MuiDrawer from "@mui/material/Drawer";
-import IconButton from "@mui/material/IconButton";
-import List from "@mui/material/List";
-import { CSSObject, styled, Theme, useTheme } from "@mui/material/styles";
-import Typography from "@mui/material/Typography";
+import LogoClosed from "@frontend/public/assets/easterEggButton/logo-closed.svg";
+import LogoOpen from "@frontend/public/assets/easterEggButton/logo-open.svg";
+import {
+  Box,
+  CssBaseline,
+  Divider,
+  Drawer as MuiDrawer,
+  IconButton,
+  List,
+  Typography,
+  useTheme,
+} from "@mui/material";
+import { CSSObject, styled, Theme } from "@mui/material/styles";
 import {
   BookOpenIcon,
-  DoorOpenIcon,
+  LayoutGridIcon,
   MapIcon,
   PanelLeftIcon,
 } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import * as React from "react";
+import React from "react";
 
 import DarkModeToggle from "./DarkModeToggle";
-import SidebarItem from "./SidebarIconButton";
+import SidebarItem from "./SidebarItem";
 
 const drawerWidth = 250;
 
 const openedMixin = (theme: Theme): CSSObject => ({
   width: drawerWidth,
+  backgroundColor: theme.palette.background.default,
+  color: theme.palette.text.primary,
   transition: theme.transitions.create("width", {
     easing: theme.transitions.easing.sharp,
     duration: theme.transitions.duration.enteringScreen,
@@ -34,162 +39,123 @@ const openedMixin = (theme: Theme): CSSObject => ({
 });
 
 const closedMixin = (theme: Theme): CSSObject => ({
+  backgroundColor: theme.palette.background.default,
+  color: theme.palette.text.primary,
   transition: theme.transitions.create("width", {
     easing: theme.transitions.easing.sharp,
     duration: theme.transitions.duration.leavingScreen,
   }),
   overflowX: "hidden",
-  width: `calc(${theme.spacing(7)} + 1px)`,
-  [theme.breakpoints.up("sm")]: {
-    width: `calc(${theme.spacing(8)} + 1px)`,
-  },
+  width: theme.spacing(7.5), // 60px
 });
-
-const DrawerHeader = styled("div")(({ theme }) => ({
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "flex-end",
-  padding: "8px 16px",
-}));
-
-const SidebarFooter = styled("div")`
-  display: flex;
-  flex-direction: column;
-  height: 100%;
-  justify-content: flex-end;
-  padding: 10px 16px;
-  gap: 8px;
-`;
 
 const Drawer = styled(MuiDrawer, {
   shouldForwardProp: (prop) => prop !== "open",
-})(({ theme }) => ({
+})<{ open: boolean }>(({ theme, open }) => ({
   width: drawerWidth,
-  flexShrink: 0,
   whiteSpace: "nowrap",
   boxSizing: "border-box",
-  variants: [
-    {
-      props: ({ open }) => open,
-      style: {
-        ...openedMixin(theme),
-        "& .MuiDrawer-paper": openedMixin(theme),
-      },
-    },
-    {
-      props: ({ open }) => !open,
-      style: {
-        ...closedMixin(theme),
-        "& .MuiDrawer-paper": closedMixin(theme),
-      },
-    },
-  ],
+  ...(open ? openedMixin(theme) : closedMixin(theme)),
+  "& .MuiDrawer-paper": open ? openedMixin(theme) : closedMixin(theme),
+  backgroundColor: theme.palette.background.default,
 }));
 
-const mainMenuItems: Array<{
-  title: string;
-  link: string;
-  icon: React.ReactElement;
-}> = [
-  { title: "Browse Buildings", link: "/browse", icon: <BookOpenIcon /> },
-  { title: "View Map", link: "/map", icon: <MapIcon /> },
-  { title: "All Free Rooms", link: "/allRooms", icon: <DoorOpenIcon /> },
+const navItems = [
+  {
+    label: "Browse Buildings",
+    href: "/browse",
+    icon: <BookOpenIcon size={20} />,
+  },
+  {
+    label: "View Map",
+    href: "/map",
+    icon: <MapIcon size={20} />,
+  },
+  {
+    label: "All Rooms",
+    href: "/allRooms",
+    icon: <LayoutGridIcon size={20} />,
+  },
 ];
 
-export default function Sidebar({ children }: { children: React.ReactNode }) {
+const Sidebar = ({ children }: { children: React.ReactNode }) => {
   const theme = useTheme();
-  const [open, setOpen] = React.useState(true);
-  const path = usePathname();
+  const pathname = usePathname();
+  const [sidebarOpen, setSidebarOpen] = React.useState(true);
+  const [logoHover, setLogoHover] = React.useState(false); // NEW
 
-  const handleDrawerOpen = () => {
-    setOpen(true);
-  };
-
-  const handleDrawerClose = () => {
-    setOpen(false);
-  };
+  const toggleSidebar = () => setSidebarOpen((prev) => !prev);
 
   return (
     <Box sx={{ display: "flex" }}>
       <CssBaseline />
-      <Drawer variant="permanent" open={open}>
-        <DrawerHeader>
-          <Box
-            display="flex"
-            alignItems="center"
-            justifyContent="initial"
-            width={"100%"}
-          >
-            <IconButton
-              disableRipple
-              href="/"
-              LinkComponent={Link}
-              sx={[
-                {
-                  minWidth: 0,
-                  justifyContent: "center",
-                  borderRadius: "0px",
-                  ml: -0.5,
-                },
-                open ? { mr: 5 } : { mr: "auto" },
-              ]}
-            >
-              <Image height={40} src={LogoOpen} alt="Freerooms Logo" priority />
-            </IconButton>
-            <Typography
-              variant="h6"
-              noWrap
-              fontWeight={700}
-              marginLeft={-1.5}
-              sx={[open ? { opacity: 1 } : { opacity: 0 }]}
-            >
-              Freerooms
-            </Typography>
-          </Box>
-        </DrawerHeader>
-        <Divider />
-        <List sx={{ padding: "10px 16px" }}>
-          {mainMenuItems.map(({ title, link, icon }) => (
+      <Drawer variant="permanent" open={sidebarOpen}>
+        <Box
+          display="flex"
+          alignItems="center"
+          justifyContent={sidebarOpen ? "space-between" : "center"}
+          px={theme.spacing(2)}
+          py={theme.spacing(2)}
+        >
+          {sidebarOpen && (
+            <Link href="/" passHref legacyBehavior>
+              <Box
+                component="a"
+                display="flex"
+                alignItems="center"
+                gap={theme.spacing(1.5)}
+                pl={theme.spacing(1.5)}
+                sx={{
+                  textDecoration: "none",
+                  color: theme.palette.text.primary,
+                  transition: "opacity 0.3s",
+                  "&:hover": {
+                    opacity: 0.75,
+                  },
+                }}
+                onMouseEnter={() => setLogoHover(true)}
+                onMouseLeave={() => setLogoHover(false)}
+              >
+                <Image
+                  src={logoHover ? LogoClosed : LogoOpen}
+                  alt="Logo"
+                  height={32}
+                />
+                <Typography variant="h6" fontWeight={600} ml={2}>
+                  Freerooms
+                </Typography>
+              </Box>
+            </Link>
+          )}
+          <IconButton onClick={toggleSidebar}>
+            <PanelLeftIcon size={20} color={theme.palette.text.primary} />
+          </IconButton>
+        </Box>
+
+        <Divider sx={{ backgroundColor: theme.palette.divider }} />
+
+        <List sx={{ px: theme.spacing(1), pt: theme.spacing(1) }}>
+          {navItems.map(({ label, href, icon }) => (
             <SidebarItem
-              key={title}
+              key={href}
               icon={icon}
-              label={title}
-              href={link}
-              sidebarOpen={open}
-              active={path === link}
+              label={label}
+              href={href}
+              sidebarOpen={sidebarOpen}
+              active={pathname === href}
             />
           ))}
         </List>
-        <SidebarFooter>
-          <DarkModeToggle sidebarOpen={open} />
-          <Divider />
-          <Box
-            display="flex"
-            justifyContent={open ? "space-between" : "center"}
-            alignItems="center"
-            width="100%"
-          >
-            {open && (
-              <Typography
-                variant="caption"
-                display="block"
-                sx={{ color: theme.palette.text.secondary }}
-              >
-                © DevSoc {new Date().getFullYear()}, v1.0.0
-              </Typography>
-            )}
-            <IconButton
-              onClick={open ? handleDrawerClose : handleDrawerOpen}
-              sx={{ borderRadius: "8px" }}
-            >
-              <PanelLeftIcon />
-            </IconButton>
-          </Box>
-        </SidebarFooter>
+
+        <Box flexGrow={1} />
+
+        <Box px={theme.spacing(1)} pb={theme.spacing(2)}>
+          <DarkModeToggle sidebarOpen={sidebarOpen} />
+        </Box>
       </Drawer>
-      <Box component="main" sx={{ flexGrow: 1 }}>
-        {children}
-      </Box>
+      {children}
     </Box>
   );
-}
+};
+
+export default Sidebar;
