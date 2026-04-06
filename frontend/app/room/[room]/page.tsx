d0dc578f9a4954a2884f13669ad35b93ee338695
Chris Wong <123799877+xleonx0x@users.noreply.github.com>
2026-04-06 19:38:04 +1000

    chore: merge prod to dev (#700)

--- frontend/app/room/[room]/page.tsx
+++ frontend/app/room/[room]/page.tsx
@@ -113,15 +113,15 @@ const RoomPageHeader: React.FC<{ room: Room; buildingName: string }> = ({
     : "This room is managed externally by its associated school. Please contact the school to request a booking";
 
   const ratings = useRoomRatings(room.id);
-  let ratingValue = 0;
-
-  if (ratings.ratings && ratings.ratings.length > 0) {
-    ratings.ratings.forEach((rating) => {
-      ratingValue += rating.overall;
-    });
-    ratingValue = ratingValue / ratings.ratings.length;
-    ratingValue = Math.round(ratingValue * 10) / 10;
-  }
+  const ratingValue = (() => {
+    // round rating to nearest .5 if a rating exists
+    if (!ratings || !ratings.data) return 0;
+    const rating = ratings.data.overallRating;
+    const frac = rating % 1;
+    return frac >= 0.3 && frac <= 0.7
+      ? Math.floor(rating) + 0.5
+      : Math.round(rating);
+  })();
 
   return (
     <Stack
