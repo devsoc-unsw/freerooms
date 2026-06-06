6d4e16f004e1332ee4909963d44facee2972079f
Cormac <cormac@LAPTOP-0C8K460R.localdomain>
2026-06-06 16:44:36 +1000

    feat: add useBookmarks.ts hook file and functions

---
+++ frontend/hooks/useBookmarks.ts
@@ -0,0 +1,84 @@
+"use client";
+
+import React, {
+    createContext,
+    useCallback,
+    useMemo,
+    useSyncExternalStore,
+} from "react";
+
+import { Provider as ReduxProvider } from "react-redux";
+import store from "../redux/store";
+
+// const getBookmarksSnapshot = (): string[] => {
+//     const bookmarks = localStorage.getItem("bookmarks");
+
+//     return bookmarks ? JSON.parse(bookmarks) : [];
+// };
+
+// type BookmarkContextType {
+//     bookmarks: string[];
+//     isBookmarked: (roomId: string) => boolean;
+//     toggleBookmark: (roomId: string) => void;
+// };
+
+// export const BookmarkContext = createContext({
+//     bookmarks: [],
+//     isBookmarked: () => false,
+//     toggleBookmark: () => {},
+// });
+
+
+const subscribeBookmarks = (callback: () => void) => {
+    const handleStorage = (event: StorageEvent) => {
+        if (event.key === "bookmarks") callback(); // 
+    };
+
+    window.addEventListener("storage", handleStorage);
+    return () => {
+        window.removeEventListener("storage", handleStorage);
+    }
+};
+
+const getClientBookmarkSnapshot = (): string[] => {
+  const stored = window.localStorage.getItem("bookmarks");
+  if (!stored) return [];
+
+  // ...
+  return JSON.parse(stored);
+};
+
+const getServerBookmarkSnapshot = (): string[] => [];
+
+export default function useBookmarks() {
+    const bookmarks = useSyncExternalStore( // External database from react.
+        subscribeBookmarks, // tells when bookmarks change
+        getClientBookmarkSnapshot, // read current bookmarks
+        getServerBookmarkSnapshot // if we are not in the browser yet, what should the value be (????)
+    )
+
+    const isBookmarked = (roomId: string) => {
+        return bookmarks.includes(roomId);
+    };
+
+    const toggleBookmark = (roomId: string) => {
+        let nextBookmarks: string[];
+        if (isBookmarked(roomId)) {
+            nextBookmarks = bookmarks.filter((id) => id !== roomId)
+        } else {
+            nextBookmarks = [...bookmarks, roomId];
+        }
+
+        window.localStorage.setItem("bookmarks", JSON.stringify(nextBookmarks));
+        
+        // Manually trigger event so current page refreshes immediately
+        window.dispatchEvent(new StorageEvent("storage", { key: "bookmarks" }));
+    };
+
+
+    return {
+        bookmarks,
+        isBookmarked,
+        toggleBookmark,
+    };
+}
