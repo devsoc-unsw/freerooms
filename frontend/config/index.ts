612300cc5687322031d80b3c49bede1b890c6d6a
Ryan Yensch <ry2005@outlook.com>
2026-09-06 11:55:37 +1000

    fix: added restricted mapbox fallback key

--- frontend/config/index.ts
+++ frontend/config/index.ts
@@ -18,8 +18,9 @@ const API_CONFIG: Record<string, string> = Object.freeze({
 
 export const API_URL: string = API_CONFIG[env] + "/api";
 
+// Fall back to restricted public key if none is provided (only works on *.devsoc.app)
 export const MAPBOX_ACCESS_TOKEN =
-  process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN ?? "";
+  process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN || "pk.eyJ1IjoiZGV2c29jIiwiYSI6ImNtdG1sd3FjZjA5c2QyeXB1MXRjZDloNTYifQ.odb02EqDnplMwcmFVJ_e8g";
 
 export const MAPBOX_STYLE_LIGHT =
   process.env.NEXT_PUBLIC_MAPBOX_STYLE_LIGHT ??
