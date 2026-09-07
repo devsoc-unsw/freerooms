4b0bb7cc59e80551855baacb96b21a23a8d70782
Ryan Yensch <ry2005@outlook.com>
2026-09-08 02:44:59 +1000

    Changed base layout to use new theme

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,3 +1,4 @@
+import { DM_Sans } from "next/font/google";
 import "mapbox-gl/dist/mapbox-gl.css";
 import "../styles/globals.css";
 
@@ -5,6 +6,12 @@ import { Metadata } from "next";
 import React from "react";
 
 import ClientLayout from "./clientLayout";
+
+const dmSans = DM_Sans({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-dm-sans",
+});
 
 export const metadata: Metadata = {
   title: "Freerooms",
@@ -20,7 +27,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>
+      <body className={dmSans.variable}>
         <ClientLayout>{children}</ClientLayout>
       </body>
     </html>
