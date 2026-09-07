13ef72b653d0bcfe88793b697644a3cb716fe327
Ryan Yensch <ry2005@outlook.com>
2026-09-08 02:02:47 +1000

    feat: added base sapcing for components

---
+++ frontend/theme/spacing.ts
@@ -0,0 +1,15 @@
+export const space = {
+    xs: 4,
+    sm: 8,
+    md: 16,
+    lg: 24,
+    xl: 36,
+    xxl: 48,
+} as const;
+
+
+export const radius = {
+    md: 4,
+    lg: 8,
+    landing: 24,
+} as const;
